836211ed98e27ff51014bbeaac66b44eb8f0d45d
pipe34150 <pipe34150@hotmail.com>
2019-01-07 14:50:46 +0700

    [REMOVED] details in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react'
 import Background from '../components/Core/Background'
 import Contact from '../components/Contact/Index'
-import Headline , {Subtitle} from '../components/Core/Texts'
-import TextBox from '../components/Core/TextBox'
 import Home from '../components/Home/index'
+import App from '../components/Core/App';
 
 export default class componentName extends Component {
   render() {
     return (
       <div>
         <Home/>
-        <Headline>test 2</Headline>
-        <Subtitle>abc</Subtitle>
-        <TextBox text = "Boo" /> 
+        <App />
       </div>
     )
   }
