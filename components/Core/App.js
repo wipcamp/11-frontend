62de5130ba4bbcc6dcf5d331426bff5d53a59bef
pipe34150 <pipe34150@hotmail.com>
2019-01-07 14:02:05 +0700

    [ADD] App.js

---
+++ components/Core/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import What from '../What/index'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <What />
+      </div>
+    )
+  }
+}
+
