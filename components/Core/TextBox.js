ce9ca3015a6799b41a99648ab5f722074826d409
pipe34150 <pipe34150@hotmail.com>
2019-01-07 10:24:22 +0700

    [REMOVED] TextStyle from Textbox.js

--- components/Core/TextBox.js
+++ components/Core/TextBox.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import fonts from '../../config/fonts'
-import colors from '../../config/color'
+import {TextBoxStyle} from './Texts'
 
 const BoxStyle = styled.div`
     background: papayawhip;
@@ -9,17 +8,12 @@ const BoxStyle = styled.div`
     padding-top : 2em;
     padding-bottom : 2em;
 `
-const TextStyle = styled.p`
-    font-size : ${fonts.TextBox};
-    color : ${colors.TextBox};
-
-`
 const TextBox = (props) => (
     <div className = "container-fluid">
         <div className = "row justify-content-center">
             <div className = "col-md-8 col-md-offset-4">
                 <BoxStyle>
-                    <TextStyle>{props.text}</TextStyle>
+                    <TextBoxStyle>{props.text}</TextBoxStyle>
                 </BoxStyle>
             </div>
         </div>
