636df7ca2250d6ed4ba84e13d773a96fd489bc7b
pipe34150 <pipe34150@hotmail.com>
2019-01-07 14:02:33 +0700

    [ADD] MainButton.js

---
+++ components/Core/MainButton.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const MainButton = styled.img`
+    width: 40px;
+    cursor:pointer;
+    position: absolute;
+
+`
+export const NextButton = styled.img`
+    width: 40px;
+    cursor:pointer;
+    right: 0px;
+    position: absolute;
+`
+
+export const PreviousButton = styled.img`
+    width: 40px;
+    cursor:pointer;
+    left: 0px;
+    position: absolute;
+
+`
+export default NextButton 
