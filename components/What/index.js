3a9dd985c15ef327bc70320e15224213fa886c7a
pipe34150 <pipe34150@hotmail.com>
2019-01-07 14:04:07 +0700

    [ADD] What Page

---
+++ components/What/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import TextBox from '../Core/TextBox';
+import NextButton from '../Core/MainButton';
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <TextBox text = "This is What Section" />
+      </div>
+    )
+  }
+}
