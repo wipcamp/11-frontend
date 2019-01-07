c951ff676c01cbdc5835f68bd0ce6544fa0c1c4c
pipe34150 <pipe34150@hotmail.com>
2019-01-07 10:23:27 +0700

    [UPDATE] TextBoxStyle in Texts.js

--- components/Core/Texts.js
+++ components/Core/Texts.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components'
-import fonts from '../../config/fonts'
+import font from '../../config/fonts'
+import color from '../../config/color'
 
 const Headline = styled.p`
-  font-size: ${fonts.Headlines};
+  font-size: ${font.Headlines};
   font-weight : bold;
   text-align : center;
 `
 export const Subtitle = styled.p`
-  font-size : ${fonts.Subtitle}  
+  font-size : ${font.Subtitle};
+`
+
+export const TextBoxStyle = styled.p`
+  font-size : ${font.TextBox};
+  color : ${color.TextBox};
 `
 export default Headline
