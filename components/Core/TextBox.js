436d491d3cee4a3f5c7c34635128a5ab9383cc34
pipe34150 <pipe34150@hotmail.com>
2019-01-07 14:02:53 +0700

    [UPDATE] TextBox with button

--- components/Core/TextBox.js
+++ components/Core/TextBox.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import {TextBoxStyle} from './Texts'
+import {NextButton ,PreviousButton} from './MainButton'
 
 const BoxStyle = styled.div`
     background: papayawhip;
     text-align : center;
     padding-top : 2em;
     padding-bottom : 2em;
+    position: absolute;
+    display:block;
+    width:100%;
+    max-width: 700px;
+
+    @media(max-width:1024px){
+        max-width : 650px;
+    }
 `
 const TextBox = (props) => (
     <div className = "container-fluid">
         <div className = "row justify-content-center">
-            <div className = "col-md-8 col-md-offset-4">
                 <BoxStyle>
+                    <NextButton src = 'static/img/button.png'  />
+                    <PreviousButton src = 'static/img/button.png'  />
                     <TextBoxStyle>{props.text}</TextBoxStyle>
                 </BoxStyle>
-            </div>
+            
         </div>
     </div>
 )
