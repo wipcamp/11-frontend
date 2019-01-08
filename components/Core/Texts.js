8413e88c2052f96361386278a0bd77beb83dad02
pipe34150 <pipe34150@hotmail.com>
2019-01-08 16:28:52 +0700

    [UPDATE] Texts.js

--- components/Core/Texts.js
+++ components/Core/Texts.js
@@ -3,12 +3,24 @@ import font from '../../config/fonts'
 import color from '../../config/color'
 
 const Headline = styled.p`
-  font-size: ${font.Headlines};
+  font-size: ${font.Headline};
   font-weight : bold;
-  text-align : center;
 `
 export const Subtitle = styled.p`
   font-size : ${font.Subtitle};
+`
+
+export const ParagraphBold = styled.p`
+  font-size :${font.Paragraph};
+  font-weight : bold;
+`
+
+export const Paragraph = styled.p`
+  font-size :${font.Paragraph};
+`
+
+export const Small = styled.p`
+  font-size : ${font.Small};
 `
 
 export const TextBoxStyle = styled.p`
