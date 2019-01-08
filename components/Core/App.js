f06f0d0adfc72d2f2d8df471fc51251b34843b80
pipe34150 <pipe34150@hotmail.com>
2019-01-08 16:32:33 +0700

    [ADD] Relative Postition in App.js

--- components/Core/App.js
+++ components/Core/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
-
+import styled from 'styled-components'
 import What from '../What/index'
 import Who from '../Who/index'
+import Navbar from './Navbar';
 
+const Relative = styled.div`
+  position:relative;
+`
 export default class App extends Component {
   render() {
     return (
       <div>
-        <What />
-        <Who />
+        <Relative>
+          <Navbar />
+          <What />
+
+        </Relative>
       </div>
     )
   }
