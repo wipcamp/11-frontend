9b49b6bbb074014f41f3b45954cdf35d09cd3cd1
Unknows <firstpan555@gmail.com>
2019-01-08 13:14:59 +0700

    [Update] index.js don't change.And Change at App.js

--- components/Core/App.js
+++ components/Core/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
 
 import What from '../What/index'
+import Who from '../Who/index'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <What />
+        <Who />
       </div>
     )
   }
