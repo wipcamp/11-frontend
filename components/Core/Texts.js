a20d39579ac15b976f721e485ca16af9fcbb213f
pipe34150 <pipe34150@hotmail.com>
2019-01-08 16:46:37 +0700

    [UPDATE] Font-family

--- components/Core/Texts.js
+++ components/Core/Texts.js
@@ -5,26 +5,32 @@ import color from '../../config/color'
 const Headline = styled.p`
   font-size: ${font.Headline};
   font-weight : bold;
+  font-family: 'Sarabun', sans-serif;
 `
 export const Subtitle = styled.p`
   font-size : ${font.Subtitle};
+  font-family: 'Sarabun', sans-serif;
 `
 
 export const ParagraphBold = styled.p`
   font-size :${font.Paragraph};
   font-weight : bold;
+  font-family: 'Sarabun', sans-serif;
 `
 
 export const Paragraph = styled.p`
   font-size :${font.Paragraph};
+  font-family: 'Sarabun', sans-serif;
 `
 
 export const Small = styled.p`
   font-size : ${font.Small};
+  font-family: 'Sarabun', sans-serif;
 `
 
 export const TextBoxStyle = styled.p`
   font-size : ${font.TextBox};
   color : ${color.TextBox};
+  font-family: 'Sarabun', sans-serif;
 `
 export default Headline
