cc39a918e36a7e9445c6f908506c3f222dc0e6ba
YOURNAME <your@email.com>
2019-01-09 20:40:05 +0700

    [UPDATE] Responsive Who page (non finish) 2

--- components/Who/index.js
+++ components/Who/index.js
@@ -6,23 +6,7 @@ import Headline, { Subtitle, Paragraph, Small } from '../Core/Texts'
 const BoxStyle = styled.div`
   background: #d29ea4;
   text-align : left;
-  width: 85%;
-  
-    @media (min-width:320px) {
-      padding : 1.5em 1em;
-      width: 100%;
-    }
-    
-    @media (min-width:576px) {
-      padding : 1em 2em;
-      width : 100%;
-      
-    }
-    
-    @media (min-width:1024px) {
-      padding : 2em 5em;
-      width : 85%;
-    }
+  width: 100%;
 `
 
 const TextBoxColor = styled.div`
@@ -78,9 +62,9 @@ const Layout = styled.div`
   padding:2em;
 `
 export default class componentName extends Component {
-  render() {
+  render () {
     return (
-      <div className="container">
+      <div className="container px-3">
         {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. */}
         <div className="row">
           <Head className="text-center col-12">
@@ -88,30 +72,29 @@ export default class componentName extends Component {
             <Subtitle>ค่ายนี้เหมาะสำหรับใคร?</Subtitle>
           </Head>
         </div>
-        <div className="row">
-          <div className="container">
-            <div className="col-12">
-              <BoxStyle className="container">
+        <div className="container">
+          <div className="row justify-content-center">
+
+            <BoxStyle className="col-12 col-sm-12 col-md-12 col-lg-10 p-5">
+              <TextBoxColor>
+                <Icon src="static/img/icon-who/computer.png" alt="computer" />
+              </TextBoxColor>
+              <div className="my-5">
                 <TextBoxColor>
-                  <Icon src="static/img/icon-who/computer.png" alt="computer" />
+                  <Icon src="static/img/icon-who/graduate.png" alt="การศึกษา" />
                 </TextBoxColor>
-                <div className="my-5">
-                  <TextBoxColor>
-                    <Icon src="static/img/icon-who/graduate.png" alt="การศึกษา" />
-                  </TextBoxColor>
-                </div>
-                <div className="my-5">
-                  <TextBoxColor>
-                    <Icon src="static/img/icon-who/person.png" alt="เหมาะสำหรับ" />
-                  </TextBoxColor>
-                </div>
-                <div className="my-5">
-                  <TextBoxColor>
-                    <Icon src="static/img/icon-who/moon.png" alt="ระยะเวลา" />
-                  </TextBoxColor>
-                </div>
-              </BoxStyle>
-            </div>
+              </div>
+              <div className="my-5">
+                <TextBoxColor>
+                  <Icon src="static/img/icon-who/person.png" alt="เหมาะสำหรับ" />
+                </TextBoxColor>
+              </div>
+              <div className="my-5">
+                <TextBoxColor>
+                  <Icon src="static/img/icon-who/moon.png" alt="ระยะเวลา" />
+                </TextBoxColor>
+              </div>
+            </BoxStyle>
           </div>
         </div>
       </div>
