fedb708f9a0005fffea9e75d7400181e72856f5b
pipe34150 <pipe34150@hotmail.com>
2019-01-09 21:57:40 +0700

    [UPDATE] Section with NavBar

--- components/Core/App.js
+++ components/Core/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react'
+import styled from 'styled-components'
+
 import Home from '../Home'
 import What from '../What'
 import Navbar from './NavBar'
@@ -6,16 +8,35 @@ import Who from '../Who'
 import Where from '../Where'
 import When from '../When'
 
+import { Element } from 'react-scroll';
+
+const Section = styled(Element)`
+  padding: 20px 0px;
+  text-align: center;
+
+  @media(max-width: 768px) {
+    padding: 25px 0;
+  }
+`
+
 export default class App extends Component {
   render () {
     return (
       <div>
         <Navbar />
         <Home />
-        <What />
-        <Who />
-        <Where />
-        <When />
+        <Section name = "what">
+          <What />
+        </Section>
+        <Section name = "who">
+          <Who />
+        </Section>
+        <Section name = "where" >
+          <Where />
+        </Section>
+        <Section name = "when" >
+          <When />
+        </Section>
       </div>
     )
   }
