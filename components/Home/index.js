a62b501abd39852f77b5dbd9e1b73501e9673e7f
yunxyinx <yaowanat.yy@mail.kmutt.ac.th>
2019-01-09 15:24:07 +0700

    <ADD> Logo WIP CAMP

--- components/Home/index.js
+++ components/Home/index.js
@@ -13,10 +13,26 @@ const Logo = styled.img`
     width: 55%;
   }
 `
+const LogoWip = styled.img`
+    position: relative;
+    z-index: 8;
+    width: 100%;
+    margin-top: 8vh;
+    animation-name: bcCCNc;
+    animation-duration: 2s;
+    @media (min-width:801px) {
+    width: 50%;
+  }
+  @media (min-width:1024px) {
+    width: 55%;
+  }
+}
+`
 export default class componentName extends Component {
-  render () {
+  render() {
     return (
-      <div className = "text-center">
+      <div className="text-center">
+        <LogoWip src="static/img/logofinals.png" alt="WIP CAMP" />
         <Logo src="static/img/LogoCana.png" alt=" WIP Camp #11 ,คณะเทคโนโลยีสารสนเทศ , มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี " />
       </div>
     )
