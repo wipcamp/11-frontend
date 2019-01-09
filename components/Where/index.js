b13a1c88a6320ffb46fb58fa9429e2170df599c8
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-01-10 00:11:01 +0700

    [UPDATE] Responsive Where page (non finish)

--- components/Where/index.js
+++ components/Where/index.js
@@ -21,18 +21,43 @@ const Head = styled.div`
 text-align : center;
 
 `
+const BoxStyle = styled.div`
+    background: #304151;
+   
+
+    @media (min-width:320px) {
+      padding : 1.5em 1em;
+    }
+    
+    @media (min-width:576px) {
+      padding : 1em 2em;
+    }
+    
+    @media (min-width:1024px) {
+      padding : 2em 5em;
+    }
+`
+const Layout = styled.div`  
+  margin-top: 4.5%;
+  margin-bottom: 10%;
+  border: .2em solid transparent;
+  border-radius: 2em;
+  padding:2em;
+`
 export default class index extends Component {
   render () {
     return (
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-12 col-sm-12 col-md-12 col-lg-12">
+          <Layout className="col-12 col-sm-12 col-md-12 col-lg-12">
             <Head className="text-center col-12">
               <Headline>Where</Headline>
               <Subtitle>ค่ายนี้จัดที่ไหน?</Subtitle>
             </Head>
-            <Map src='static/img/Map22.png' alt="map" />
-          </div>
+            <BoxStyle>
+              <Map src='static/img/Map22.png' alt="map" />
+            </BoxStyle>
+          </Layout>
         </div>
       </div>
     )
