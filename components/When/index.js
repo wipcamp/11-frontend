9b7c64b2bbfa34f52234a2ed5106fae5b2b8f534
Unknows <firstpan555@gmail.com>
2019-01-09 14:13:24 +0700

    [UPDATE]Arrange code

--- components/When/index.js
+++ components/When/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import ImgChangeByDay from './ImgChangeByDay'
 
 export default class componentName extends Component {
-  render() {
+  render () {
     return (
       <div>
         <ImgChangeByDay datetime="2019-01-8"/>
