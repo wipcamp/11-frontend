075922bbd4a16850d3cdf5f20e3c3e6dc5cfea16
Unknows <firstpan555@gmail.com>
2019-01-10 17:21:34 +0700

    [UPDATE]Responsive not finish

--- components/Contact/Fbfeed.js
+++ components/Contact/Fbfeed.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import { FacebookProvider, Page } from 'react-facebook'
+
+const Box = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+`
 
 export default class Example extends Component {
   render () {
     return (
-      <FacebookProvider appId="1778641378908399">
-        <Page href="https://www.facebook.com/wipcamp/" tabs="timeline" />
-      </FacebookProvider>
+      <Box>
+        <FacebookProvider appId="1778641378908399">
+          <Page href="https://www.facebook.com/wipcamp/" tabs="timeline" />
+        </FacebookProvider>
+      </Box>
     )
   }
 }
