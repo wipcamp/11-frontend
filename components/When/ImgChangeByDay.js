f129a6a6348f20b64472b2ab7d17ea3d7e70c078
Unknows <firstpan555@gmail.com>
2019-01-10 16:20:38 +0700

    [UPDATE]Responsive when

--- components/When/ImgChangeByDay.js
+++ components/When/ImgChangeByDay.js
@@ -14,38 +14,59 @@ const BoxStyle = styled.div`
     background: papayawhip;
     text-align : center;
 `
+const Img = styled.img`
+    width:7em;
+    @media (min-width:320px) {
+      width:4em;
+    }
+    @media (min-width:420px) {
+    
+      width:5em;
+    }
+    @media (min-width:576px) {
+      margin-top: 2%;
+      width:5em;
+    }
+    @media (min-width:768px) {
+      margin-top: 5%;
+      width:7em;
+    }
+    @media (min-width:1024px) {
+      margin-top: 5%;
+      padding: .2em;
+      width:7em;
+    }
+`
 
 const ImgChangeDay = () => (
-  <div className="container-fluid">
-    <div className="row">
-      <BoxStyle className="col-12 col-sm-6 col-xl-3">
-            <img src={date1.isBefore(datenow) ? '../../static/img/wippo/mochi.png' : '../../static/img/wippo/sakura.png'} />
-          <span>
-            <TextBoxLong text="กำหนดเปิดรับสมัคร" textday="8 มกราคม 2562 - 13 มกราคม 2562"/>
-          </span>
-      </BoxStyle>
-      <div class="w-auto"></div>
-      <BoxStyle className="col-12 col-sm-6 col-xl-3">
-        <img src={date2.isBefore(datenow) ? '../../static/img/wippo/mochi.png' : '../../static/img/wippo/sakura.png'} />
-        <span>
-          <TextBoxLong text="กำหนดปิดรับสมัคร" textday="13 มกราคม 2562"/>
-        </span>
-      </BoxStyle>
-      <div class="w-auto"></div>
-      <BoxStyle className="col-12 col-sm-6 col-xl-3">
-        <img src={date3.isBefore(datenow) ? '../../static/img/wippo/mochi.png' : '../../static/img/wippo/sakura.png'} />
-        <span>
-          <TextBoxLong text="วันประกาศผล" textday="21 มกราคม 2562"/>
-        </span>
-      </BoxStyle>
-      <div class="w-auto"></div>
-      <BoxStyle className="col-12 col-sm-6 col-xl-3">
-        <img src={date4.isBefore(datenow) ? '../../static/img/wippo/mochi.png' : '../../static/img/wippo/sakura.png'} />
-        <span>
-          <TextBoxLong text="วันเปิดค่าย" textday="1 กุมภาพันธ์"/>
-        </span>
-      </BoxStyle>
-    </div>
+  <div className="row">
+    <BoxStyle className="col-12 col-sm-6 col-xl-3">
+      <Img src={date1.isBefore(datenow) ? '../../static/img/wippo/mochi.png' : '../../static/img/wippo/sakura.png'} />
+      <span>
+        <TextBoxLong text="กำหนดเปิดรับสมัคร" textday="8 มกราคม 2562 - 13 มกราคม 2562"/>
+      </span>
+    </BoxStyle>
+    <div class="w-auto"></div>
+    <BoxStyle className="col-12 col-sm-6 col-xl-3">
+      <Img src={date2.isBefore(datenow) ? '../../static/img/wippo/mochi.png' : '../../static/img/wippo/sakura.png'} />
+      <span>
+        <TextBoxLong text="กำหนดปิดรับสมัคร" textday="13 มกราคม 2562"/>
+      </span>
+    </BoxStyle>
+    <div class="w-auto"></div>
+    <BoxStyle className="col-12 col-sm-6 col-xl-3">
+      <Img src={date3.isBefore(datenow) ? '../../static/img/wippo/mochi.png' : '../../static/img/wippo/sakura.png'} />
+      <span>
+        <TextBoxLong text="วันประกาศผล" textday="21 มกราคม 2562"/>
+      </span>
+    </BoxStyle>
+    <div class="w-auto"></div>
+    <BoxStyle className="col-12 col-sm-6 col-xl-3">
+      <Img src={date4.isBefore(datenow) ? '../../static/img/wippo/mochi.png' : '../../static/img/wippo/sakura.png'} />
+      <span>
+        <TextBoxLong text="วันเปิดค่าย" textday="1 กุมภาพันธ์"/>
+      </span>
+    </BoxStyle>
   </div>
 )
 
--- components/When/index.js
+++ components/When/index.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
 import ImgChangeByDay from './ImgChangeByDay'
-import Headline, { Subtitle, Paragraph, Small } from '../Core/Texts'
+import Headline, { Subtitle } from '../Core/Texts'
 import styled from 'styled-components'
 // import Textbox from './TextBoxLong'
 const Head = styled.div`
-text-align : center;
-
+  text-align : center;
 `
 
 export default class componentName extends Component {
   render () {
     return (
-      <div>
+      <div className="container p-5">
         <Head className="text-center col-12">
           <Headline>When</Headline>
           <Subtitle>ค่ายนี้จัดเมื่อไร?</Subtitle>
