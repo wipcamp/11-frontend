fb81b9cf606263b0ea79c0d6b506b0c0154d5412
pipe34150 <pipe34150@hotmail.com>
2019-01-10 16:18:41 +0700

    [UPDATE] Config file Loading.js as Preload

--- components/Core/Loading.js
+++ components/Core/Loading.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class Loading extends Component {
   render() {
     return (
-      <div>
-        Loading
+      <div className = "container">
+        <div className = "row justify-content-center">
+          Loading
+        </div>
       </div>
     )
   }
