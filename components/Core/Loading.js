9d0663a93158b52e46f554a8f3d2c3cbb2fd011d
pipe34150 <pipe34150@hotmail.com>
2019-01-11 16:41:46 +0700

    [UPDATE] Centered Loading Section

--- components/Core/Loading.js
+++ components/Core/Loading.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
+const Background = styled.div`
+  height: 100vh;
+  background-color: papayawhip;
+`
+
+const Position = styled.p`
+  padding-top:50vh;
+`
 export default class Loading extends Component {
-  render() {
+  render () {
     return (
-      <div className = "container">
-        <div className = "row justify-content-center">
-          Loading
+      <Background>
+        <div className = "container">
+          <div className=" row justify-content-center align-items-center">
+            <Position>
+              Loading ..
+            </Position>
+          </div>
         </div>
-      </div>
+      </Background>
     )
   }
 }
