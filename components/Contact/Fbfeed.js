e108effe7af698b2426912c26d478ca51538fac7
Unknows <firstpan555@gmail.com>
2019-01-12 16:27:45 +0700

    [UPDATE]Increase border size

--- components/Contact/Fbfeed.js
+++ components/Contact/Fbfeed.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { FacebookProvider, Page, Feed } from 'react-facebook'
+import { FacebookProvider, Page } from 'react-facebook'
 
 const Box = styled.div`
   width: 100%;
   height: 100%;
-  border : solid 10px white;
+  border : solid 21px white;
   border-radius:20px;
 `
 
@@ -14,7 +14,7 @@ export default class Example extends Component {
     return (
       <Box>
         <FacebookProvider appId="1778641378908399">
-          <Page href="https://www.facebook.com/wipcamp/" width="510px" tabs="timeline" />
+          <Page href="https://www.facebook.com/wipcamp/" width="500px" tabs="timeline" />
         </FacebookProvider>
       </Box>
     )
