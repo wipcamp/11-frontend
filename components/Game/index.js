5696ea121e8e6d288d798e2737cc788b7c103ec8
Unknows <firstpan555@gmail.com>
2019-01-12 17:05:34 +0700

    [UPDATE]Layout Game Page

--- components/Game/index.js
+++ components/Game/index.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
+import Headline, { Subtitle, Paragraph, Small, Font } from '../Core/Texts'
+import { PDBox } from '../Core/ResponBox';
+
+const BoxStyle = styled(PDBox)`
+    background: rgba(196, 196, 196, 0.73);
+`
+
+const Layout = styled.div`  
+  margin-top: 4.5%;
+  margin-bottom: 10%;
+  border: .2em solid transparent;
+  border-radius: 2em;
+  padding:2em;
+`
+
+const Head = styled.div`
+  text-align : center;
+`
 export default class index extends Component {
-  render() {
+  render () {
     return (
-      <div>
-        Game
+      <div className="container">
+        <div className="row justify-content-center text-center">
+          <Layout className="col-12 col-sm-12 col-md-12 col-lg-12">
+            <Head className="col-12">
+              <Headline>Game</Headline>
+              <Subtitle>สนุกนะเล่นสิ</Subtitle>
+            </Head>
+            <BoxStyle>
+            </BoxStyle>
+          </Layout>
+        </div>
       </div>
     )
   }
