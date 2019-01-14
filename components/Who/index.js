5f7323013ea65d558199b1f3e1f170159a83035c
pipe34150 <pipe34150@hotmail.com>
2019-01-14 13:33:09 +0700

    [REMOVE] Components in Who

--- components/Who/index.js
+++ components/Who/index.js
@@ -21,17 +21,8 @@ const Icon = styled(IconBox)`
 `
 
 const Head = styled.div`
-text-align : center;
-
+  text-align : center;
 `
-const Layout = styled.div`  
-  margin-top: 4.5%;
-  margin-bottom: 10%;
-  border: .2em solid transparent;
-  border-radius: 2em;
-  padding:2em;
-`
-
 const IconInfo = [
   { icon: '/static/img/icon-who/moon.png', text: 'น้อง ๆ ที่สามารถอยู่ร่วมค่ายค้างคืนได้ตลอดระยะเวลา 5 วัน 4 คืน' },
   { icon: '/static/img/icon-who/graduate.png', text: 'น้อง ๆ มัธยมศึกษาตอนปลายแผนการเรียนวิทย์-คณิต ศิลป์-คำนวณ' },
@@ -43,7 +34,6 @@ export default class componentName extends Component {
   render () {
     return (
       <div className="container">
-        {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. */}
         <div className="row">
           <Head className="text-center col-12">
             <Headline>Who</Headline>
