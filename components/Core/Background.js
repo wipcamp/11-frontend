2d965e0f089fbe2e3ccfd40e1e5a1632d40893ce
pipe34150 <pipe34150@hotmail.com>
2019-01-17 15:43:03 +0700

    [UPDATE] Fixed Background in their section

--- components/Core/Background.js
+++ components/Core/Background.js
@@ -2,7 +2,11 @@ import styled from 'styled-components'
 
 const Background = styled.div`
   background-image:url(${props => props.bgImageUrl});
-  height:100%;
+  min-height: 100vh;
   background-repeat:no-repeat;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
 `
 export default Background
