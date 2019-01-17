956792670bd06c69f3f6a7597ba02ee45e59c221
Unknows <firstpan555@gmail.com>
2019-01-17 18:54:54 +0700

    [ADD]Responsive Model

--- components/Core/Model.js
+++ components/Core/Model.js
@@ -1,7 +1,26 @@
 import styled from 'styled-components'
 
-const Model = styled.img`
+const Model = styled.div`
     content:url(${props => props.wippo});
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 10em;
+    @media(min-width: 320px) {
+        width: 5em;
+    }
+    @media(min-width: 420px) {
+        width: 7em;
+    }
+    @media(min-width: 576px) {
+        width: 7em;
+    }
+    @media(min-width: 768px) {
+        width: 10em;
+    }
+    @media(min-width: 1024px) {
+        width: 10em;
+    }
 `
 
 export default Model
