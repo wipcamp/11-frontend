28b1ce5c4ea0e110e3fe1e2d9ccaa48eb17bcfdd
Unknows <firstpan555@gmail.com>
2019-01-17 18:19:35 +0700

    [UPDATE]Arrange code

--- components/Who/index.js
+++ components/Who/index.js
@@ -37,13 +37,12 @@ export default class componentName extends Component {
         <div className="row">
           <Head className="text-center col-12">
             <Headline>Who</Headline>
-            
           </Head>
         </div>
         <div className="container">
           <div className="row justify-content-center">
             <BoxStyle className="col-12 col-sm-12 col-md-12 col-lg-10 p-5">
-            <Subtitle className="row justify-content-center"><b>ค่ายนี้เหมาะสำหรับใคร?</b></Subtitle>
+              <Subtitle className="row justify-content-center"><b>ค่ายนี้เหมาะสำหรับใคร?</b></Subtitle>
               {IconInfo.map((data, i) => (
                 <div className="my-5">
                   <TextBoxColor>
