5d53cff62afb1db990c4766bf91d8cbaebc40498
pipe34150 <pipe34150@hotmail.com>
2019-01-18 21:39:18 +0700

    [UPDATE] Responsive Logo in Home Page

--- components/Home/index.js
+++ components/Home/index.js
@@ -7,6 +7,9 @@ const Logo = styled.img`
   position: relative;
   width: 100%;
   margin-top: 0vh;
+  @media (min-width:300px) {
+    width: 80%;
+  }
   @media (min-width:801px) {
     width: 50%;
   }
@@ -19,8 +22,6 @@ const LogoWip = styled.img`
   z-index: 8;
   width: 100%;
   margin-top: 8vh;
-  animation-name: bcCCNc;
-  animation-duration: 2s;
   @media (min-width:801px) {
     width: 50%;
   }
@@ -29,13 +30,13 @@ const LogoWip = styled.img`
   }
 `
 export default class componentName extends Component {
-  render() {
+  render () {
     return (
       <Background bgImageUrl="static/img/home7.png">
         <div className="text-center">
           <LogoWip src="static/img/Logo.png" alt="WIP CAMP" />
           <Logo src="static/img/LogoCana.png" alt=" WIP Camp #11 ,คณะเทคโนโลยีสารสนเทศ , มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี " />
-          <Model className="center" wippo='static/img/wippo/original-1.png' alt="wippo" /> 
+          <Model className="center" wippo='static/img/wippo/original-1.png' alt="wippo" />
         </div>
       </Background>
     )
