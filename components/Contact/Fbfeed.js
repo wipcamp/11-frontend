b73a758d29622540c7e0b0fbd011b70992654319
pipe34150 <pipe34150@hotmail.com>
2019-01-20 02:17:11 +0700

    [REMOVE] Box Component

--- components/Contact/Fbfeed.js
+++ components/Contact/Fbfeed.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { FacebookProvider, Page } from 'react-facebook'
 
-const Box = styled.div`
-  width: 100%;
-  height: 100%;
-`
-
 export default class Example extends Component {
   render () {
     return (
-      <Box>
+      <div>
         <FacebookProvider appId="1778641378908399">
           <Page href="https://www.facebook.com/wipcamp/" width="350" height="350" tabs="timeline" />
         </FacebookProvider>
-      </Box>
+      </div>
     )
   }
 }
