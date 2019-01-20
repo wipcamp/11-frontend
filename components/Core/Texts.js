ff5d73c625fd73435d769f818f46bf83171ba23d
pipe34150 <pipe34150@hotmail.com>
2019-01-20 23:53:44 +0700

    [UPDATE] Text from <p> to <div>

--- components/Core/Texts.js
+++ components/Core/Texts.js
@@ -2,39 +2,39 @@ import styled from 'styled-components'
 import font from '../../config/fonts'
 import color from '../../config/color'
 
-const Headline = styled.p`
+const Headline = styled.div`
   font-size: ${font.Headline};
   font-weight : bold;
   text-align : center;
 `
-export const Subtitle = styled.p`
+export const Subtitle = styled.div`
   font-size : ${font.Subtitle};
   
 `
 
-export const ParagraphBold = styled.p`
+export const ParagraphBold = styled.div`
   font-size :${font.Paragraph};
   font-weight : bold;
   
 `
 
-export const Paragraph = styled.p`
+export const Paragraph = styled.div`
   font-size :${font.Paragraph};
   
 `
 
-export const Small = styled.p`
+export const Small = styled.div`
   font-size : ${font.Small};
   
 `
 
-export const TextBoxStyle = styled.p`
+export const TextBoxStyle = styled.div`
   font-size : ${font.TextBox};
   color : ${color.TextBox};
   
 `
 
-export const Font = styled.p`
+export const Font = styled.div`
   @media (min-width:320px) {
     font-size: 0.8em;
   }
