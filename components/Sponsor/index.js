eb715d0443e6927dc51b41ef1be23472460cf5d3
pipe34150 <pipe34150@hotmail.com>
2019-01-20 16:33:07 +0700

    [ADD] Sponsor Page

---
+++ components/Sponsor/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import Headline, { Subtitle, Paragraph, Small, Font } from '../Core/Texts'
+import { PDBox } from '../Core/ResponBox'
+import Model from '../Core/Model'
+
+export default class index extends Component {
+  render () {
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-10">
+            <div className="row justify-content-center">
+              <Headline>Sponsors</Headline>
+              <PDBox>
+                <div className="col-12">
+                  <div className="row justify-content-center">
+                    <Subtitle className="mb-4"><b>ผู้สนับสนุน</b></Subtitle>
+                  </div>
+                </div>
+              </PDBox>
+            </div>
+            <Model className="center" wippo='static/img/wippo/PurplePotato.png' alt="wippo" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
