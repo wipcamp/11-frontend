40baebd348b551dda7b75ba8ea5ce5d97c838956
Unknows <firstpan555@gmail.com>
2019-01-20 15:16:36 +0700

    [UPDATE]Layout

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -6,16 +6,12 @@ import Model from '../Core/Model'
 import { WDBox, MTBox, PDBox } from '../Core/ResponBox'
 import Logo from './Logo'
 import Fbfeed from './Fbfeed'
-import TextBoxBackground from '../Core/TextBoxBackground'
-
-const BoxStyle = styled(PDBox)`
-  background: rgba(196, 196, 196, 0.4);
-  text-align : left;
-  width: 100%;
-`
 
 const FbBox = styled(WDBox)`
     width: 500px;
+    @media (min-width:576px) {
+      text-align:center;
+    }
     @media (min-width:768px) {
       text-align:center;
     }
@@ -43,19 +39,24 @@ const Div1 = styled(MTBox)`
       margin-left:10em;
     }
     @media (min-width:768px) {
-      margin-left:16em;
+      margin-left:18em;
     }
     @media (min-width:1024px) {
-      /* margin-top:rem; */
       margin-left :10em;
     }
 `
 const Div2 = styled.div`
- @media (min-width:320px) {
-   margin-right:2em;   
- }
+    @media (min-width:320px) {
+      margin-left:-1em;   
+    }
+    @media (min-width:420px) {
+      margin-left:-1em;   
+    }
+    @media (min-width:576px) {
+      margin-left:.5em;   
+    }
     @media (min-width:768px) {
-      margin-left:2em;
+      margin-left:10em;
     }
 `
 
@@ -78,10 +79,6 @@ const Img = styled.img`
     }
 `
 
-const Head = styled.div`
-  text-align : center;  
-`
-
 const DivCon = styled.div`
   display: inline-block;
 `
@@ -101,67 +98,68 @@ const TextA = styled.a`
   }
 `
 
+const PDBox1 = styled(PDBox)`
+  background-color : unset;
+`
 export default class index extends Component {
   render () {
     return (
       <div className="container">
-        <div className="row">
-          <Head className="text-center col-12">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-10">
             <Headline>Contact</Headline>
-          </Head>
-        </div>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-12 col-sm-12 col-md-12 col-lg-1 col-xl-1"></div>
-            <BoxStyle className="row col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10">
-              <Subtitle className = "text-center col-12"><b>ติดต่อเรา</b></Subtitle>
-              <FbBox className="pt-4 pb-4 mr-5 col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
-                <Fbfeed />
-              </FbBox>
-              <Div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
-                <Div2 className="row justify-content-start">
-                  <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                    <Logo img="../../static/img/social/instagram.svg" link="https://www.instagram.com/wipcamp/" />
-                  </div>
-                  <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                    <Logo img="../../static/img/social/twitter.svg" link="https://twitter.com/WIPCamp" />
-                  </div>
-                  <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                    <Logo img="../../static/img/social/youtube.svg" link="https://www.youtube.com/channel/UC_Lhso9PxSlxNuw2wG9zYIA" />
-                  </div>
-                  <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                    <Logo img="../../static/img/social/line@.svg" link="http://line.naver.jp/ti/p/Z_Qg6KlFnU#~" />
-                  </div>
-                </Div2>
-                <div className="row">
-                  <Div1 className="mt-4 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                    <DivCon>
-                      <a href="tel://02-107-3576"><Img src="../../static/img/social/phone.svg" /></a>
-                    </DivCon>
-                    <DivCon>
+            <PDBox1 className="pr-1 pl-1">
+              <div className="row justify-content-center">
+                <div className="row col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 p-0">
+                  <Subtitle className = "text-center col-12"><b>ติดต่อเรา</b></Subtitle>
+                  <FbBox className="pt-4 pb-4 mr-5 col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                    <Fbfeed />
+                  </FbBox>
+                  <Div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                    <Div2 className="row justify-content-center">
+                      <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                        <Logo img="../../static/img/social/instagram.svg" link="https://www.instagram.com/wipcamp/" />
+                      </div>
+                      <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                        <Logo img="../../static/img/social/twitter.svg" link="https://twitter.com/WIPCamp" />
+                      </div>
+                      <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                        <Logo img="../../static/img/social/youtube.svg" link="https://www.youtube.com/channel/UC_Lhso9PxSlxNuw2wG9zYIA" />
+                      </div>
+                      <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                        <Logo img="../../static/img/social/line@.svg" link="http://line.naver.jp/ti/p/Z_Qg6KlFnU#~" />
+                      </div>
+                    </Div2>
+                    <div className="row">
+                      <Div1 className="mt-4 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                        <DivCon>
+                          <a href="tel://02-107-3576"><Img src="../../static/img/social/phone.svg" /></a>
+                        </DivCon>
+                        <DivCon>
                   &nbsp;&nbsp;
-                    </DivCon>
-                    <DivCon>
-                      <Paragraph><TextA href="tel://02-107-3576">02-107-3576</TextA></Paragraph>
-                    </DivCon>
-                  </Div1>
-                  <Div1 className="mt-4 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                    <DivCon>
-                      <a href="mailto:wippo@wipcamp.com"><Img src="../../static/img/social/mail.svg" /></a>
-                    </DivCon>
-                    <DivCon>
+                        </DivCon>
+                        <DivCon>
+                          <Paragraph><TextA href="tel://02-107-3576">02-107-3576</TextA></Paragraph>
+                        </DivCon>
+                      </Div1>
+                      <Div1 className="mt-4 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                        <DivCon>
+                          <a href="mailto:wippo@wipcamp.com"><Img src="../../static/img/social/mail.svg" /></a>
+                        </DivCon>
+                        <DivCon>
                   &nbsp;&nbsp;
-                    </DivCon>
-                    <Font>
-                      <Paragraph><TextA href="mailto:wippo@wipcamp.com">wippo@wipcamp.com</TextA></Paragraph>
-                    </Font>
-                  </Div1>
+                        </DivCon>
+                        <Font>
+                          <Paragraph><TextA href="mailto:wippo@wipcamp.com">wippo@wipcamp.com</TextA></Paragraph>
+                        </Font>
+                      </Div1>
+                    </div>
+                  </Div>
                 </div>
-              </Div>
-            </BoxStyle>
-            <div className="col-12 col-sm-12 col-md-12 col-lg-1 col-xl-1"></div>
+              </div>
+            </PDBox1>
+            <Model className="center" wippo='static/img/wippo/PurplePotato.png' alt="wippo" />
           </div>
-          <Model className="center" wippo='static/img/wippo/PurplePotato.png' alt="wippo" />
         </div>
       </div>
     )
