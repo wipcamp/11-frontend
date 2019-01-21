b41d525f705c48201d4f65022a3e87baefc6d5d2
pipe34150 <pipe34150@hotmail.com>
2019-01-21 14:42:04 +0700

    [UPDATE] Background and icon in What

--- components/What/index.js
+++ components/What/index.js
@@ -4,16 +4,17 @@ import styled from 'styled-components'
 import Headline, { Subtitle, Paragraph, Small, Font } from '../Core/Texts'
 import { PDBox, IconBox } from '../Core/ResponBox'
 import Model from '../Core/Model'
+import Background from '../Core/Background'
 
 const Icon = styled(IconBox)`
   width:7em;
 `
 
 const IconInfo = [
-  { icon: '/static/img/icon-what/java (2).png', text: 'Java Programming', t: 'Java Programming' },
-  { icon: '/static/img/icon-what/IT fun (1).png', text: 'IT Fundamentals', t: 'IT Fundamentals' },
-  { icon: '/static/img/icon-what/network (2).png', text: 'Network', t: 'Network' },
-  { icon: '/static/img/icon-what/web (2).png', text: 'HTML5&CSS3', t: 'HTML5&CSS3' }
+  { icon: '/static/img/icon-what/w11.png', text: 'Java Programming', t: 'Java Programming' },
+  { icon: '/static/img/icon-what/w12.png', text: 'IT Fundamentals', t: 'IT Fundamentals' },
+  { icon: '/static/img/icon-what/w13.png', text: 'Network', t: 'Network' },
+  { icon: '/static/img/icon-what/w14.png', text: 'HTML5&CSS3', t: 'HTML5&CSS3' }
 ]
 
 const WhatText = [
@@ -25,43 +26,45 @@ const WhatText = [
 export default class index extends Component {
   render () {
     return (
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-12 col-sm-12 col-md-12 col-lg-10">
-            <div className="row">
-              <div className="text-center col-12">
-                <Headline>What</Headline>
-              </div>
-              <PDBox>
-                <div className="col-12">
-                  <div className="row justify-content-center">
-                    <Subtitle className="mb-4"><b>ค่ายนี้คืออะไร ?</b></Subtitle>
-                  </div>
-                  <Paragraph>
-                    { WhatText.map((info, i) => (
-                      <Font key = {i}>
+      <Background bgImageUrl="static/img/what.png">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-sm-12 col-md-12 col-lg-10">
+              <div className="row">
+                <div className="text-center col-12">
+                  <Headline>What</Headline>
+                </div>
+                <PDBox>
+                  <div className="col-12">
+                    <div className="row justify-content-center">
+                      <Subtitle className="mb-4"><b>ค่ายนี้คืออะไร ?</b></Subtitle>
+                    </div>
+                    <Paragraph>
+                      { WhatText.map((info, i) => (
+                        <Font key = {i}>
                         &nbsp;&nbsp;&nbsp;&nbsp;{info}
-                      </Font>
-                    ))
+                        </Font>
+                      ))
+                      }
+                    </Paragraph>
+                  </div>
+                  <div className="row text-center mt-2">
+                    {
+                      IconInfo.map((info, i) => (
+                        <div key={i} className="col-3 col-md-3 p-1">
+                          <Icon src={info.icon} alt={info.t} />
+                          <Small><Font>{info.text}</Font></Small>
+                        </div>
+                      ))
                     }
-                  </Paragraph>
-                </div>
-                <div className="row text-center mt-2">
-                  {
-                    IconInfo.map((info, i) => (
-                      <div key={i} className="col-3 col-md-3 p-1">
-                        <Icon src={info.icon} alt={info.t} />
-                        <Small><Font>{info.text}</Font></Small>
-                      </div>
-                    ))
-                  }
-                </div>
-              </PDBox>
+                  </div>
+                </PDBox>
+              </div>
+              <Model className="center" wippo='static/img/wippo/PurplePotato.png' alt="wippo" />
             </div>
-            <Model className="center" wippo='static/img/wippo/PurplePotato.png' alt="wippo" />
           </div>
         </div>
-      </div>
+      </Background>
     )
   }
 }
