26c354b35fb3a6ffe6f769bc0b34806ac7644fc0
pipe34150 <pipe34150@hotmail.com>
2019-01-21 14:42:24 +0700

    [UPDATE] Background in Where

--- components/Where/index.js
+++ components/Where/index.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import Headline, { Subtitle, Paragraph, Small, Font } from '../Core/Texts'
 import { PDBox } from '../Core/ResponBox'
 import Model from '../Core/Model'
+import Background from '../Core/Background'
 
 const Map = styled.img`
   height: 35%;
@@ -28,29 +29,31 @@ const WhereText = [
 export default class index extends Component {
   render () {
     return (
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-12 col-sm-12 col-md-12 col-lg-10">
-            <div className="row justify-content-center">
-              <Headline>Where</Headline>
-              <PDBox>
-                <div className="col-12">
-                  <div className="row justify-content-center">
-                    <Subtitle className="mb-4"><b>ค่ายนี้จัดที่ไหน?</b></Subtitle>
+      <Background bgImageUrl = "static/img/where.png">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-sm-12 col-md-12 col-lg-10">
+              <div className="row justify-content-center">
+                <Headline>Where</Headline>
+                <PDBox>
+                  <div className="col-12">
+                    <div className="row justify-content-center">
+                      <Subtitle className="mb-4"><b>ค่ายนี้จัดที่ไหน?</b></Subtitle>
+                    </div>
+                    <Small>
+                      {WhereText.map((info, i) => (
+                        <Font key = {i} className = "row justify-content-center m-1">{info}</Font>
+                      ))}
+                    </Small>
+                    <a className = "row justify-content-center" href="https://goo.gl/j6GJ21" target="_blank" ><Map src='static/img/mapWithWippo.png' alt="map" /></a>
                   </div>
-                  <Small>
-                    {WhereText.map((info, i) => (
-                      <Font key = {i} className = "row justify-content-center m-1">{info}</Font>
-                    ))}
-                  </Small>
-                  <a className = "row justify-content-center" href="https://goo.gl/j6GJ21" target="_blank" ><Map src='static/img/mapWithWippo.png' alt="map" /></a>
-                </div>
-              </PDBox>
+                </PDBox>
+              </div>
+              <Model className="center" wippo='static/img/wippo/PurplePotato.png' alt="wippo" />
             </div>
-            <Model className="center" wippo='static/img/wippo/PurplePotato.png' alt="wippo" />
           </div>
         </div>
-      </div>
+      </Background>
     )
   }
 }
