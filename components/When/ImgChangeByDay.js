c18d61868b6a0c05193b02f0d54fbc8ce0ced70d
yunxyinx <yaowanat.yy@mail.kmutt.ac.th>
2019-01-25 02:00:00 +0700

    [UPDATE] Rename model and change wippo

--- components/When/ImgChangeByDay.js
+++ components/When/ImgChangeByDay.js
@@ -39,7 +39,7 @@ const ImgChangeDay = () => (
   <div className="row justify-content-center">
     <div className="row col-12 col-sm-12 col-lg-3 col-md-3 col-xl-3 justify-content-center">
       <div>
-        <Img src={date1.isBefore(datenow) ? '../../static/img/wippo/original-1.png' : '../../static/img/wippo/original-2.png'} />
+        <Img src={date1.isBefore(datenow) ? '../../static/img/wippo/wippoWhat.png' : '../../static/img/wippo/original-2.png'} />
       </div>
       <Font className="p-0 col-6 col-sm-6 col-lg-12 col-md-12 col-xl-12 text-center">
         <TextBoxLong text="กำหนดเปิดรับสมัคร" textday="8 มกราคม 2562"/>
@@ -47,7 +47,7 @@ const ImgChangeDay = () => (
     </div>
     <div className="row col-12 col-sm-12 col-lg-3 col-md-3 col-xl-3 justify-content-center">
       <div>
-        <Img src={date2.isBefore(datenow) ? '../../static/img/wippo/original-1.png' : '../../static/img/wippo/original-2.png'} />
+        <Img src={date2.isBefore(datenow) ? '../../static/img/wippo/wippoWho.png' : '../../static/img/wippo/original-2.png'} />
       </div>
       <Font className="p-0 col-6 col-sm-6 col-lg-12 col-md-12 col-xl-12 text-center">
         <TextBoxLong text="กำหนดปิดรับสมัคร" textday="11 มกราคม 2562"/>
@@ -55,7 +55,7 @@ const ImgChangeDay = () => (
     </div>
     <div className="row col-12 col-sm-12 col-lg-3 col-md-3 col-xl-3 justify-content-center">
       <div>
-        <Img src={date3.isBefore(datenow) ? '../../static/img/wippo/original-1.png' : '../../static/img/wippo/original-2.png'} />
+        <Img src={date3.isBefore(datenow) ? '../../static/img/wippo/wippoWhere.png' : '../../static/img/wippo/original-2.png'} />
       </div>
       <Font className="p-0 col-6 col-sm-6 col-lg-12 col-md-12 col-xl-12 text-center">
         <TextBoxLong text="วันประกาศผล" textday="14 มกราคม 2562"/>
@@ -63,7 +63,7 @@ const ImgChangeDay = () => (
     </div>
     <div className="row col-12 col-sm-12 col-lg-3 col-md-3 col-xl-3 justify-content-center"> 
       <div>
-        <Img src={date4.isBefore(datenow) ? '../../static/img/wippo/original-1.png' : '../../static/img/wippo/original-2.png'} />
+        <Img src={date4.isBefore(datenow) ? '../../static/img/wippo/wippoGame.png' : '../../static/img/wippo/original-2.png'} />
       </div>
       <Font className="p-0 col-6 col-sm-6 col-lg-12 col-md-12 col-xl-12 text-center">
         <TextBoxLong text="วันเปิดค่าย" textday="17 มกราคม 2562"/>
