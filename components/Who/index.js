7d83ec2bedc54b530cb968bc22c7d17fcb768a4f
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-01-25 16:35:44 +0700

    [REMOVE] TextBoxWho and FontWho from who page to ResponBox

--- components/Who/index.js
+++ components/Who/index.js
@@ -1,49 +1,9 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-
-import Headline, { Subtitle, Paragraph, Small, Font } from '../Core/Texts'
-import { PDBox, IconBox } from '../Core/ResponBox'
+import Headline, { Paragraph, FontWho } from '../Core/Texts'
+import { PDBox, TextBoxWho, IconWho  } from '../Core/ResponBox'
 import Model from '../Core/Model'
 import Background from '../Core/Background'
-
-const TextBoxColor = styled.div`
-  background: #172D38;
-
-  @media (min-width:320px) {
-    padding : 0.35em 0.35em;
-    margin : 1em 0em ;
-  }
-  @media (min-width:412px) {
-    padding : 1em 1em;
-    margin : 1 0em ;
-  }
-   @media (min-width:576px) {
-    padding : 1em 2em;
-  }
-  @media (min-width:768px) {
-    padding : 1em 1em;
-    margin : 0 0.5em ;
-  }
-  @media (min-width:1024px) {
-    padding : 0em 1em;
-  }
-`
-
-const Icon1 = styled.img`
-  @media (min-width:320px) {
-    width: 45px;
-    height: 45px;
-  }
-  @media (min-width:412px) {
-    width: 65px;
-    height: 65px;
-  }
-
-  @media (min-width:1024px) {
-    width: 70px;
-    height: 70px;
-  }
- `
 
 const TextPosition = styled.div`  
   margin-left:1em;
@@ -52,35 +12,7 @@ const TextPosition = styled.div`
     transform: translate(0, 50%);
   }
 `
-export const Font1 = styled.p`
-color : #fff;
-  @media (min-width:320px) {
-    margin-left : 8px;
-    margin-top : 2px;
-    font-size: 12px;
-  }
-  @media (min-width:412px) {
-    font-size: 14px;
-    margin-left : 20px;
-    margin-top : 10px;
-  }
-  @media (min-width:576px) {
-    font-size: 15px;
-    margin-left : 20px;
-    margin-top : 5px;
-  }
-  @media (min-width:768px) {
-    font-size: 15px;
-    margin-left : -25px;
-    margin-top : 10px;
-  }
-  @media (min-width:1024px) {
-    font-style: normal;
-    font-size: 16px;
-    margin-left : -25px;
-    margin-top : 13px;
-  }
-`
+
 const IconInfo = [
   { icon: '/static/img/icon-who/moon.png', text: 'น้อง ๆ ที่สามารถอยู่ร่วมค่ายค้างคืนได้ตลอดระยะเวลา 5 วัน 4 คืน' },
   { icon: '/static/img/icon-who/graduate.png', text: 'น้อง ๆ มัธยมศึกษาตอนปลายแผนการเรียนวิทย์-คณิต ศิลป์-คำนวณ' },
@@ -100,20 +32,20 @@ export default class componentName extends Component {
                 <PDBox>
                   {IconInfo.map((data, i) => (
                     <div className="my-2 mx-1" key={i}>
-                      <TextBoxColor>
+                      <TextBoxWho>
                         <div className="row">
                           <div className ="col-2" >
-                            <Icon1 key={i} src={data.icon} />
+                            <IconWho key={i} src={data.icon} />
                           </div>
                           <div className="col-10 pl-0">
                             <TextPosition>
                               <Paragraph>
-                                <Font1 key={i}>{data.text}</Font1>
+                                <FontWho key={i}>{data.text}</FontWho>
                               </Paragraph>
                             </TextPosition>
                           </div>
                         </div>
-                      </TextBoxColor>
+                      </TextBoxWho>
                     </div>
                   ))}
                 </PDBox>
