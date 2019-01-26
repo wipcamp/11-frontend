f1feaab1ea4734c944c96fa7f140e899e0e4059c
pipe34150 <pipe34150@hotmail.com>
2019-01-26 23:15:12 +0700

    [UPDATE] Wippo Position in FAQs

--- components/FAQs/index.js
+++ components/FAQs/index.js
@@ -90,7 +90,7 @@ export default class Question extends Component {
               </div>
               </div>
             </div>
-                <Model wippo='wippoFAQs.png' alt="wippo" />
+            <Model wippo='wippoFAQs.png' alt="wippo" />
           </div>
         </div>
       </Background>
