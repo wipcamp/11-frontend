8644e82bc4000a1cd2f0c10cf5762dcc50ddde87
Unknows <firstpan555@gmail.com>
2019-01-26 21:11:07 +0700

    [UPDATE] Wippo layout

--- components/Core/Model.js
+++ components/Core/Model.js
@@ -4,25 +4,28 @@ const Model = styled.div`
     content:url('../../static/img/wippo/${props => props.wippo}');
     display: block;
     z-index:10;
-    margin-top: -10em;
     margin-left: auto;
     margin-right: auto;
     width: 10em;
-    text-align: center !important;
 
     @media(min-width: 320px) {
+        margin-top: -5em;
         width: 5em;
     }
     @media(min-width: 420px) {
+        margin-top: -6em;
         width: 7em;
     }
     @media(min-width: 576px) {
+        margin-top: -6em;
         width: 7em;
     }
     @media(min-width: 768px) {
+        margin-top: -7em;
         width: 10em;
     }
     @media(min-width: 1024px) {
+        margin-top: -10em;
         width: 18em;
     }
 `
