87c678903f4f4be81a563527c7f22935db0f89f2
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-01-28 16:51:59 +0700

    [UPDATE] Responsive background finish eiei

--- components/Core/Background.js
+++ components/Core/Background.js
@@ -2,6 +2,13 @@ import styled from 'styled-components'
 
 const Background = styled.div`
   background-image:url(${props => props.bgImageUrl});
+  min-height: 100vh;
+  background-repeat:no-repeat;
+  background-position: center center;
+  background-attachment: fixed;
+  background-size: cover;
+  padding-top:2em;
+  
     @media(min-width:320px) {
     padding-top:4em;
     background-size: 160%;
