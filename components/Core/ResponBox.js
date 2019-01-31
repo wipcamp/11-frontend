31feaf75f24dc2321682b6318a2a86b20e24dfd8
pipe34150 <pipe34150@hotmail.com>
2019-01-31 11:06:11 +0700

    [UPDATE] Margin PDBox Responsive

--- components/Core/ResponBox.js
+++ components/Core/ResponBox.js
@@ -26,7 +26,7 @@ box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
   @media (min-width:320px) {
     padding : 1.5em 1em;
-    margin : 2em 0.5em ;
+    margin : 1em 0.5em ;
   }
    @media (min-width:576px) {
     padding : 1em 2em;
