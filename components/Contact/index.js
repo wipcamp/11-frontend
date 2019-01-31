369febe6cd921c93c3e427e4287032a52b00d236
pipe34150 <pipe34150@hotmail.com>
2019-01-31 11:30:29 +0700

    [UPDATE] Subtitle in Contact

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -37,10 +37,10 @@ export default class index extends Component {
               <h1><Headline>Contact</Headline></h1>
             </div>
             <PDBox1>
-              <div className="row justify-content-center">
                 <div className="text-center">
                   <Subtitle>ติดต่อเรา</Subtitle>
                 </div>
+              <div className="row justify-content-center">
                 <div className="col-12 col-sm-12 col-lg-6 mb-3">
                   <Fbfeed/>
                 </div>
