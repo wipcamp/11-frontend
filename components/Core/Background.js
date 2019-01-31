257140c7d49360a8125221e3b9bfa32d6661585c
pipe34150 <pipe34150@hotmail.com>
2019-01-31 20:13:23 +0700

    [UPDATE] Background styled

--- components/Core/Background.js
+++ components/Core/Background.js
@@ -2,28 +2,19 @@ import styled from 'styled-components'
 
 const Background = styled.div`
   background-image:url(${props => props.bgImageUrl});
-  min-height: 100vh;
-  background-repeat:no-repeat;
-  background-position: center center;
-  background-attachment: fixed;
-  background-size: cover;
-  padding-top:2em;
   
-    @media(min-width:320px) {
+  @media(min-width:320px) {
     padding-top:1.5em;
     background-size: 160%;
     background-position-y: 100%;
-    
   }
   @media(min-width:1024px) {
     min-height: 100vh;
-   background-repeat:no-repeat;
-   background-position: center center;
-   background-attachment: fixed;
-   background-size: cover;
-   padding-top:2em;
-    
+    background-repeat:no-repeat;
+    background-position: center center;
+    background-attachment: fixed;
+    background-size: cover;
+    padding-top:2em;
   }
- 
 `
 export default Background
