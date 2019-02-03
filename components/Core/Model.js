17f6f83c26c406eee029f4b0cf741a57e078b8da
pipe34150 <pipe34150@hotmail.com>
2019-02-03 18:37:35 +0700

    [UPDATE] Model.js

--- components/Core/Model.js
+++ components/Core/Model.js
@@ -1,28 +1,15 @@
 import styled from 'styled-components'
 
-const Model = styled.div`
+const Model = styled.img`
     content:url('../../static/img/wippo/${props => props.wippo}');
     z-index:10;
     margin-left: auto;
     margin-right: auto;
-    width: 10em;
-    height:100%;
-
-    @media(min-width: 320px) {
-        width: 5em;
-    }
-    @media(min-width: 420px) {
-        width: 7em;
-    }
-    @media(min-width: 576px) {
-        width: 7em;
-    }
-    @media(min-width: 768px) {
-        width: 10em;
-    }
-    @media(min-width: 1024px) {
-        width: 18em;
-    }
+    position: absolute;
+    height: 20em;
+    bottom: 0vh;
+    left: 0;
+    right: 0;
 `
 
 export default Model
