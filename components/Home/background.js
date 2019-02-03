d7e1cd16f3dbfb8880ad95fb1a3b614a80784de5
pipe34150 <pipe34150@hotmail.com>
2019-02-03 23:56:05 +0700

    [ADD] Background.js @Home

---
+++ components/Home/background.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Layer1 = styled.img`
+  position: absolute;
+  z-index: 1;
+  bottom: 0vh;
+  left: 50%;
+  transform: translate(-50%,0%);
+  width:100%;
+
+  @media (min-width:320px) {
+    width:150%;
+  }
+  @media (min-width:412px) {
+    width:140%;
+  }
+  @media (min-width:576px) {
+    width:130%;
+  }
+  @media (min-width:768px) {
+    width:120%;
+  }
+  @media (min-width:1024px) {
+    width:100%;
+  }
+`
+const LeftBuildy = styled.img`
+  position: absolute;
+  z-index: 0;
+  bottom: 35vh;
+  left: 0vh;
+  width:35%;
+  
+  @media (min-width:320px) {
+    left:-20vh;
+    bottom: 10vh;
+    width:65%;
+  }
+  @media (min-width:412px) {
+    left:-30vh;
+    bottom: 10vh;
+    width:80%;
+  }
+  @media (min-width:576px) {
+    left:-20vh;
+    bottom: 20vh;
+    width:60%;
+  }
+  @media (min-width:768px) {
+    left:-5vh;
+    bottom: 20vh;
+    width:40%;
+  }
+  @media (min-width:1024px) {
+    bottom: 27vh;
+    width:35%;
+  }
+
+`
+const RightBuildy = styled.img`
+  position: absolute;
+  z-index: 0;
+  bottom: 35vh;
+  right: 0vh;
+  width:35%;
+
+  @media (min-width:320px) {
+    right:-20vh;
+    bottom: 10vh;
+    width:65%;
+  }
+  @media (min-width:412px) {
+    right:-30vh;
+    bottom: 10vh;
+    width:80%;
+  }
+  @media (min-width:576px) {
+    right:-20vh;
+    bottom: 20vh;
+    width:60%;
+  }
+  @media (min-width:768px) {
+    right:-5vh;
+    bottom: 20vh;
+    width:40%;
+  }
+  @media (min-width:1024px) {
+    bottom: 27vh;
+    width:35%;
+  }
+`
+
+const LeftTree = styled.img`
+  position : absolute;
+  z-index:2;
+  bottom: 0vh;
+  left : 0vw;
+  width:25%;
+`
+const RightTree = styled.img`
+  position : absolute;
+  z-index:2;
+  bottom: 0vh;
+  right: 0vw;
+  width:30%;
+`
+const CloudyRight = styled.img`
+  position : absolute;
+  z-index:0;
+  bottom: 50vh;
+  right: 0vw;
+  width:30%;
+`
+
+const Mount = styled.img`
+  position: absolute;
+  z-index: -1;
+  bottom: 10vh;
+  left: 50%;
+  transform: translate(-50%,0%);
+  width:100%;
+
+  @media (min-width:320px) {
+    width:150%;
+  }
+  @media (min-width:412px) {
+    width:140%;
+  }
+  @media (min-width:576px) {
+    width:130%;
+  }
+  @media (min-width:768px) {
+    width:120%;
+  }
+  @media (min-width:1024px) {
+    width:100%;
+  }
+`
+
+export default class background extends Component {
+  render () {
+    return (
+      <div className = "text-align-center">
+        <Layer1 src = '/static/img/background-home/MainHome_Wall.png' />
+        <LeftBuildy src = '/static/img/background-home/MainHome_LeftBuildings.png' />
+        <RightBuildy src = '/static/img/background-home/MainHome_RightBuildings.png' />
+        <LeftTree src = '/static/img/background-home/MainHome_LeftTree.png' />
+        <RightTree src = '/static/img/background-home/MainHome_RightTree.png' />
+        <Mount src = '/static/img/background-home/MainHome_Mountains.png' />
+      </div>
+    )
+  }
+}
