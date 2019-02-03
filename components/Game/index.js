08e60392c7b0176e0592c6488796f3e811dab66d
Unknows <firstpan555@gmail.com>
2019-02-03 23:47:29 +0700

    [UPDATE]จัดเรียง Code และลองใส่รูป ยังไม่ได้ปรับขนาด

--- components/Game/index.js
+++ components/Game/index.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-import Headline, { Subtitle, Paragraph, Small, Font } from '../Core/Texts'
-import { PDBox } from '../Core/ResponBox'
-import Background from '../Core/Background'
-import Model from '../Core/Model'
+// import Headline, { Subtitle, Paragraph, Small, Font } from '../Core/Texts'
+// import { PDBox } from '../Core/ResponBox'
 import Bg from '../Core/Bg'
 import Wippo from '../Core/Wippo'
 
+const Background = styled.img`
+  position: absolute;
+  z-index: 0;
+  bottom: 0vh;
+  width: 100%;
+
+  @media(min-width:320px) {
+    width:150%;
+  }
+  @media(min-width:1024px) {
+    width: 100%;
+  }
+`
 export default class index extends Component {
   render () {
     return (
       <Bg>
-        <Wippo wippo='wippoFAQs.png' />
+        {/* <Background src='../../static/img/' /> */}
+        <Wippo wippo='wippoGame.png' />
         <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-12 col-sm-12 col-md-12 col-lg-10">
-              <div className="row justify-content-center">
-                <h1><Headline>Game</Headline></h1>
-              </div>
-              <div className="row justify-content-center">
-                <PDBox>
-                  <div className="col-12">
-                    <div className="row justify-content-center">
-                      <Subtitle className="mb-4"><b>มาเล่นกันเถอะ</b></Subtitle>
-                    </div>
-                  </div>
-                </PDBox>
-              </div>
+          <div className="row">
+            <div className="col-12 d-flex justify-content-center align-items-center">
+              <a href="http://game.wip.camp" target="_blank">
+                <img alt="Gamecenter" src='../../static/img/gameCenteIcon.png' width="100%" height="100%" />
+              </a>
             </div>
           </div>
         </div>
