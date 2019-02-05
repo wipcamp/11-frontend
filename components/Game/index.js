a92ccd7f671af7cd56ffa442f4292344757a4cb1
Unknows <firstpan555@gmail.com>
2019-02-05 18:57:11 +0700

    [UPDATE]Responsive in Game

--- components/Game/index.js
+++ components/Game/index.js
@@ -8,21 +8,43 @@ import Background from './background'
 const Img = styled.img`
    @media(min-width:320px) {
     width:309px;
-    height: 164px;
+    height: auto;
+  }
+  @media (min-width:410px) {
+    width:360px;
+    height: auto;
+  }
+  @media (min-width:576px) {
+    width:390px;
+    height: auto;
+  }
+  @media (min-width:768px) {
+    width: 420px;
+    height: auto;
   }
   @media(min-width:1024px) {
-    width:650px;
-    height:345px;
+    width:500px;
+    height:auto;
   }
 `
 const Div = styled.div`
+  top:50%;
   z-index:9;
   transform: translate(0%,30%);
   @media(min-width:320px) {
-    margin-top:8em;
+    top: 35vh;
+  }
+  @media (min-width:410px) {
+    top: 35vh;
+  }
+  @media (min-width:576px) {
+    top: 25vh;
+  }
+  @media (min-width:768px) {
+    top: 20vh;
   }
   @media(min-width:1024px) {
-    margin-top:5em;
+    top: 10vh;
   }
 `
 
@@ -34,7 +56,7 @@ export default class index extends Component {
         <Wippo wippo='wippoGame.png' />
         <div className="container">
           <div className="row">
-            <Div className="pt-5 col-12 d-flex justify-content-center align-items-center text-center">
+            <Div className="col-12 d-flex justify-content-center align-items-center text-center">
               <a href="http://game.wip.camp" target="_blank">
                 <Img alt="Gamecenter" src='../../static/img/gameCenteIcon.png' />
               </a>
