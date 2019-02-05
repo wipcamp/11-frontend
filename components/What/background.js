9be837df91b5d5a41bbc3696527fe3f5d4ebe218
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-02-05 14:55:02 +0700

    [UPDATE] Responsive new object in what page

--- components/What/background.js
+++ components/What/background.js
@@ -47,36 +47,58 @@ const Layer2 = styled.img`
   }
   @media (min-width:1024px) {
     width:100%;
-    bottom:0vh;
+    
   }
 `
-const Rock = styled.img`
+const RockLeft = styled.img`
   position: absolute;
   z-index: 0;
   bottom: 0vh;
   left: 50%;
-  transform: translate(-50%,0%);
+  transform: translate(-153%,0%);
   width:100%;
 
   @media (min-width:320px) {
-    width:150%;
-    bottom:10vh;
+    width:40%;
+    bottom:8vh;
   }
   @media (min-width:412px) {
-    width:140%;
-    bottom:12vh;
+    width:40%;
+    bottom:8vh;
   }
-  @media (min-width:576px) {
-    width:130%;
-    bottom:18vh;
-  }
+  
   @media (min-width:768px) {
-    width:120%;
-    bottom:13vh;
+    width:40%;
+    bottom:9vh;
   }
   @media (min-width:1024px) {
-    width:100%;
-    bottom: 10vh;
+    width:33%;
+    bottom: 6vh;
+  }
+`
+const RockRight = styled.img`
+  position: absolute;
+  z-index: 0;
+  bottom: 0vh;
+  left: 50%;
+  transform: translate(43%,0%);
+  width:100%;
+
+  @media (min-width:320px) {
+    width:40%;
+    bottom:9vh;
+  }
+  @media (min-width:412px) {
+    width:40%;
+    bottom:8vh;
+  }
+  @media (min-width:768px) {
+    width:40%;
+    bottom:10vh;
+  }
+  @media (min-width:1024px) {
+    width:35%;
+    bottom: 8vh;
   }
 `
 const Road = styled.img`
@@ -88,50 +110,20 @@ const Road = styled.img`
   width:100%;
 
   @media (min-width:320px) {
-    width:20%;
-    bottom:20vh;
+    width:25%;
+    bottom:17vh;
   }
 
-  @media (min-width:576px) {
-    width:20%;
-  }
   @media (min-width:768px) {
     width:20%;
   }
   @media (min-width:1024px) {
     width:18%;
+    bottom:10vh;
+    
   }
 `
-const Sakura1 = styled.img`
-  position: absolute;
-  z-index: 0;
-  bottom: 0vh;
-  left: 50%;
-  transform: translate(-50%,0%);
-  width:100%;
 
-  @media (min-width:320px) {
-    width:150%;
-    bottom : 19vh;
-
-  }
-  @media (min-width:412px) {
-    width:150%;
-    bottom :21vh;
-  }
-  @media (min-width:576px) {
-    width:150%;
-    bottom:28vh;
-  }
-  @media (min-width:768px) {
-    width:150%;
-    bottom:20vh;
-  }
-  @media (min-width:1024px) {
-    bottom : 13vh;
-    width:150%;
-  }
-`
 const Tree = styled.img`
   position: absolute;
   z-index: 0;
@@ -142,11 +134,11 @@ const Tree = styled.img`
 
   @media (min-width:320px) {
     width:240%;
-    bottom:20vh;
+    bottom:18vh;
   }
   @media (min-width:412px) {
     width:210%;
-    bottom:23vh;
+    bottom:20vh;
   }
   @media (min-width:576px) {
     width:180%;
@@ -154,56 +146,24 @@ const Tree = styled.img`
   }
   @media (min-width:768px) {
     width:120%;
-    bottom:27vh;
-  }
-  @media (min-width:1024px) {
-    bottom : 22vh;
-    width:110%;
-  }
-`
-const Sakura2 = styled.img`
-  position: absolute;
-  z-index: 0;
-  bottom: 0vh;
-  left: 50%;
-  transform: translate(-50%,0%);
-  width:100%;
-
-  @media (min-width:320px) {
-    width:200%;
     bottom:25vh;
   }
-  @media (min-width:412px) {
-    width:180%;
-    bottom:28vh;
-  }
-  @media (min-width:576px) {
-    width:160%;
-    bottom:35vh;
-  }
-  @media (min-width:768px) {
-    width:140%;
-    bottom:30vh;
-  }
   @media (min-width:1024px) {
-    bottom : 25vh;
-    width:110%;
+    bottom : 20vh;
+    width:100%;
   }
 `
-
-
 export default class componentName extends Component {
   render () {
     return (
       <div className = "text-align-center">
-        <Sakura1 src = 'static/img/background-what/SakuraBack.png'/>
-        <Sakura2 src = 'static/img/background-what/SakuraFront.png'/>
-        <Tree src = 'static/img/background-what/TreeFront.png'/>
-        <Tree src = 'static/img/background-what/TreeBack.png'/>
-        <Layer2 src = 'static/img/background-what/floor.png'/>
-        <Rock src = 'static/img/background-what/rock.png'/>
         <Road src = 'static/img/background-what/road.png'/>
-        <Sakura2 src = 'static/img/background-what/SakuraFront.png'/>
+        <Tree src = 'static/img/background-what/sakuratreeEdited3.png'/>
+        <Layer2 src = 'static/img/background-what/floor.png'/>
+        
+        <RockLeft src = 'static/img/background-what/newRockLeft.png'/>
+        <RockRight src = 'static/img/background-what/newRockRight.png'/>
+        
         <Layer1 src = 'static/img/background-what/Bridge.png'/>
         
       </div>
