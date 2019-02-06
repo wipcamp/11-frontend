40e2ca5cb8a9a9056afd91bcf696a7b19a1f0ee9
Unknows <firstpan555@gmail.com>
2019-02-06 18:50:14 +0700

    [UPDATE]เปลี่ยนตำแหน่งการลิงค์ของ Bootstarp and font-family

--- pages/_document.js
+++ pages/_document.js
@@ -19,9 +19,8 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           <meta name="description" content="WIP Camp #11 : Ways to IT Professionals Camp ค่ายเส้นทางสู่ฝันนักไอที : ค่าย สำหรับน้องๆ มัธยม ปลาย ที่จะพาน้องๆมาทำความรู้จักกับ ไอที อย่างเต็มตัว ตลอดทั้ง ค่าย น้องๆจะได้รับ ความรู้ ความสนุกสนาน จากพี่ๆ ไอที บางมด แล้วเจอกันนะครับ"/>
           <meta name="keywords" content="wipcamp,itcamp,ค่ายไอที,ค่ายคอม"/>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css?family=Knewave" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css?family=Sarabun" rel="stylesheet" />
+          <link rel="stylesheet" href="../static/css/bootstrap.css" crossOrigin="anonymous" type="text/css" />
+          <link href="../static/css/font-family.css" rel="stylesheet" type="text/css" />
           <link rel="shortcut icon" href="/static/img/favicon/favicon.ico" type="image/x-icon"/>
           <link rel="apple-touch-icon" sizes="60x60" href="/fv-apple.png"/>
           <link rel="icon" href="/static/img/favicon.ico" type="/imgae/x-icon"/>
