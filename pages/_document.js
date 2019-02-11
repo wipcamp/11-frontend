11de8d40352c1447c9ce25a29c63c264b9b2ac91
Unknows <firstpan555@gmail.com>
2019-02-11 17:41:58 +0700

    [UPDATE]Delete comment in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -64,18 +64,11 @@ export default class MyDocument extends Document {
           <script src="../static/script/jquery-3.3.1.slim.min.js" type="text/javascript" rel="stylesheet" crossorigin="anonymous" />
           <script src="../static/script/bootstrap.min.js" type="text/javascript" rel="stylesheet" crossorigin="anonymous" />
           <script dangerouslySetInnerHTML={{ __html: googleTagManager }} />
-          {/* <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NFZH4C8');` }} /> */}
         </Head>
         <body className="custom_class">
           <Main />
           <NextScript />
           <noscript dangerouslySetInnerHTML={{ __html: googleNoScript }} />
-          {/* <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NFZH4C8" height="0" width="0" style="display:none;visibility:hidden;"></iframe>` }} /> */}
         </body>
       </html>
     )
