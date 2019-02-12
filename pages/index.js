e4cc9c82792049d72351199e6507e018e13b6c31
pipe34150 <pipe34150@hotmail.com>
2019-02-12 17:16:02 +0700

    [UPDATE] Rearrange Code in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import App from '../components/Core/App.js'
+import GameCenter from '../components/GameCenter/index'
 
 export default class componentName extends Component {
   render () {
