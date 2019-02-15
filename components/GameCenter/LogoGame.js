a70126fbdbca10421a1d6cb187839ff88c22421b
yunxyinx <yaowanat.yy@mail.kmutt.ac.th>
2019-02-15 15:46:13 +0700

    [ADD] Logo Game and hover

---
+++ components/GameCenter/LogoGame.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+export const LogoGame1 = styled.img`
+  position: relative;
+  width: 70%;
+  z-index: 2;
+
+  &:hover{
+    content: url('../../static/img/game-center/logoGame1_hover.png');
+  }
+
+  @media (min-width:320px) {
+    width:100%;
+  }
+  @media (min-width:412px) {
+    width:90%;
+  }
+  @media (min-width:576px) {
+    width:100%;
+  }
+  @media (min-width:768px) {
+    width:80%;
+  }
+  @media (min-width:1024px) {
+    width:70%;
+  }
+`
+
+export const LogoGame2 = styled.img`
+  position: relative;
+  width: 70%;
+  z-index: 2;
+  &:hover{
+    content: url('../../static/img/game-center/logoGame2_hover.png');
+  }
+  @media (min-width:320px) {
+    width:100%;
+  }
+  @media (min-width:412px) {
+    width:90%;
+  }
+  @media (min-width:576px) {
+    width:100%;
+  }
+  @media (min-width:768px) {
+    width:80%;
+  }
+  @media (min-width:1024px) {
+    width:70%;
+  }
+`
+export const LogoGame3 = styled.img`
+  position: relative;
+  width: 70%;
+  z-index: 2;
+  &:hover{
+    content: url('../../static/img/game-center/logoGame3_hover.png');
+  }
+  @media (min-width:320px) {
+    width:100%;
+  }
+  @media (min-width:412px) {
+    width:90%;
+  }
+  @media (min-width:576px) {
+    width:100%;
+  }
+  @media (min-width:768px) {
+    width:80%;
+  }
+  @media (min-width:1024px) {
+    width:70%;
+  }
+`
