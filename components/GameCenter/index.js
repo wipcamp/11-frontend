d874ba3c0529e22d8df0af02ea78eabedddacd9b
Unknows <firstpan555@gmail.com>
2019-02-15 22:26:27 +0700

    [UPDATE]Responsive Gamecenter

--- components/GameCenter/index.js
+++ components/GameCenter/index.js
@@ -8,6 +8,7 @@ const Bg = styled.div`
   position: relative;
   background : #F8E9D6;
   z-index:100;
+  overflow-x:hidden;
 `
 
 const Layer1 = styled.img`
@@ -79,12 +80,15 @@ const Div = styled.div`
 const TextClimbbamboo = styled(Div)`
   left:6.5vw;
   @media (min-width:320px) {
-    font-size:25px;
-    left :19vw;
+    font-size:15px;
+    left:6.5vw;
    
   }
+  @media (min-width:375px) {
+    left :5.5vw;
+  }
   @media (min-width:412px) {
-    left :12vw;
+    left :6vw;
   }
   @media (min-width:576px) {
     left:10vw;
@@ -105,30 +109,68 @@ const TextClimbbamboo = styled(Div)`
 `
 const TextJanpu = styled(Div)`
   left:7vw;
+  transform:translate(0%,0);
   @media (min-width:320px) {
-    font-size:15px;
-    left:10vw;
+    font-size:23px;
+    left:20vw;
    
   }
+  @media (min-width:360px) {
+    left:19vw;
+  }
+  @media (min-width:375px) {
+    left:16vw;
+  }
+  @media (min-width:380px) {
+    left:18vw;
+  }
   @media (min-width:412px) {
+    left:12vw;
+  }
+  @media (min-width:500px) {
     left:11vw;
-   
+  }
+  @media (min-width:550px) {
+    left:10vw;
   }
   @media (min-width:576px) {
     font-size:24px;
+    left:11.5vw;
+  }
+  @media (min-width:613px) {
+    left:10vw;
   }
   @media (min-width:700px) {
-    font-size:24px;
-    left:8vw;
+    left:9vw;
   }
-  @media (min-width:770px) {
-    font-size:20px;
-    left:8vw;
+  @media (min-width:750px) {
+    margin-top:-1.25em;
+    left:6vw;
+  }
+  @media (min-width:810px) {
+    left:5.8vw;
+  }
+  @media (min-width:836px) {
+    left:5.6vw;
+  }
+  @media (min-width:855px) {
+    left:5.4vw;
+  }
+  @media (min-width:895px) {
+    left:5.2vw;
+  }
+  @media (min-width:945px) {
+    left:5vw;
+  }
+  @media (min-width:992px) {
+    margin-top:-1.5em;
+    left:8.5vw;
   }
   @media (min-width:1024px) {
-    font-size:28px;
-    left:7vw;
-    
+    margin-top:-1.25em;
+  }
+  @media (min-width:1200px) {
+    margin-top:-1.5em;
   }
 `
 const TextShinobi = styled(Div)`
@@ -136,7 +178,6 @@ const TextShinobi = styled(Div)`
   @media (min-width:320px) {
     font-size:15px;
     left:15vw;
-   
   }
   @media (min-width:412px) {
     left:16vw;
@@ -156,30 +197,105 @@ const TextShinobi = styled(Div)`
   @media (min-width:1024px) {
     font-size:28px;
     left:9vw; 
-    
   }
+`
+
+const ScoreBox = styled.div`
+  position:absolute;
+  background-color: #f8e9d6;
+  box-shadow: 0px 4px 4px #000000;
+  width:100%;
+  height: 100px;
+  opacity:0.6;
+  z-index:200;
+  margin-top:-9px;
+  transform:translate(0,0);
+  @media (min-width:320px) {
+    margin-top:-8px;
+    width:75%;
+    height: 30px;
+    left:6vw;
+  }
+  @media (min-width:375px) {
+    width:80%;
+    left:5vw;
+  }
+  @media (min-width:412px) {
+    width:73%;
+    left:4.5vw;
+  }
+  @media (min-width:576px) {
+    width:75%;
+  }
+  @media (min-width:768px) {
+    left:2.5vw;
+    width:70%;
+  }
+  @media (min-width:1024px) {
+    left:6vw;
+    width:60%;
+  }
+  @media (min-width:1440px) {
+    left:5vw;
+    width:62%;
+  }
+  @media (min-width:1500px) {
+    left:4.7vw;
+  }
+  @media (min-width:2560px) {
+    left:3vw;
+    width:60%;
+  }
+`
+const ScoreBoxFirstGame = styled(ScoreBox)`
+  @media (min-width:320px) {
+    margin-top:-9px;
+    width:85%;
+    height: 35px;
+    left:7vw;
+  }
+  @media (min-width:375px) {
+    left:5.5vw;
+  }
+  @media (min-width:412px) {
+    width:76%;
+    left:5vw;
+  }
+  @media (min-width:576px) {
+    width:80%;
+  }
+  @media (min-width:768px) {
+    left:2.5vw;
+    width:68%;
+  }
+  
 `
 
 const SmallSize = () => (
   <Mini>
-    <div className="row">
-      <div className = "col-12">
-        <LogoGame src="../../static/img/game-center/logoGame1_comingSoon.png" />
-        <h1><TextClimbbamboo>Climb Bamboo</TextClimbbamboo></h1>
+    <div className="row justify-content-center">
+      <div className = "col-12 mb-4">
+        {/* <ScoreBox /> */}
         {/* <LogoGame src="../../static/img/game  center/logoGame1_comingSoon.png" imagehover="../../static/img/game-center/" display="block"/> */}
-
+        <LogoGameHover src="../../static/img/game-center/logoGame2.png" imagehover="../../static/img/game-center/logoGame2_hover.png" />
+        <h1><TextJanpu> Janpu Ninja </TextJanpu></h1>
+        <ScoreBoxFirstGame />
       </div>
     </div>
     <div className = "row">
       <div className = "col-6 mt-2">
+        <LogoGame src="../../static/img/game-center/logoGame1_comingSoon.png"/>
+        <h1><TextClimbbamboo>Climb Bamboo</TextClimbbamboo></h1>
         {/* <LogoGame src="../../static/img/game-center/logoGame2.png" /> */}
-        <LogoGameHover src="../../static/img/game-center/logoGame2.png" imagehover="../../static/img/game-center/logoGame2_hover.png" />
-        <h1><TextJanpu> Janpu Ninja </TextJanpu></h1>
+        <Center>
+          <ScoreBox />
+        </Center>
       </div>
       <div className = "col-6 mt-2">
         {/* <LogoGame src="../../static/img/game-center/logoGame3_comingSoon.png" imagehover="../../static/img/game-center/" />> */}
         <LogoGame src="../../static/img/game-center/logoGame3_comingSoon.png" />
         <h1><TextShinobi>Shinobi</TextShinobi></h1>
+        <ScoreBox />
       </div>
     </div>
   </Mini>
@@ -190,17 +306,20 @@ const BigSize = () => (
     <Center className = "col-4 mt-5">
       <LogoGame src="../../static/img/game-center/logoGame1_comingSoon.png" />
       <h1><TextClimbbamboo>Climb Bamboo</TextClimbbamboo></h1>
+      <ScoreBox />
       {/* <LogoGame src="../../static/img/game-center/logoGame1_comingSoon.png" imagehover="../../static/img/game-center/" /> */}
     </Center>
     <Center className = "col-4">
       <LogoGameHover src="../../static/img/game-center/logoGame2.png" imagehover="../../static/img/game-center/logoGame2_hover.png" />
       <h1><TextJanpu> Janpu Ninja </TextJanpu></h1>
+      <ScoreBox />
       {/* <LogoGame src="../../static/img/game-center/logoGame2.png" /> */}
     </Center>
     <Center className = "col-4 mt-5">
       {/* <LogoGame src="../../static/img/game-center/logoGame3_comingSoon.png" imagehover="../../static/img/game-center/" />> */}
       <LogoGame src="../../static/img/game-center/logoGame3_comingSoon.png" />
       <h1><TextShinobi>Shinobi</TextShinobi></h1>
+      <ScoreBox />
     </Center>
   </div>
 )
