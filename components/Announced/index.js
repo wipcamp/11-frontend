b50cd219ce6b5516ef4758283d0c618cdae30730
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-03-19 00:27:08 +0700

    [UPDATE] finish all object in announced page

--- components/Announced/index.js
+++ components/Announced/index.js
@@ -4,35 +4,13 @@ import Bg from '../Core/Bg'
 import Background from '../Announced/background'
 
 
-const Base = styled.img`
-position: absolute;
-z-index: 1;
-bottom: 12.5vh;
-`
-const Shadow = styled.img`
-position: absolute;
-z-index: 0;
-bottom: 0vh;
-`
-const Linen = styled.img`
-position: absolute;
-z-index: 2;
-bottom: 42.8vh;
-`
-const Scripture = styled.img`
-position: absolute;
-z-index: 3;
-bottom: 54vh;
-`
 export default class componentName extends Component {
   render () {
     return (
-      <div className="row justify-content-center">
-        <Base src='../../static/img/announce/_Scripture base.png'/>
-        <Shadow src='../../static/img/announce/_Scripture base shadow.png'/>
-        <Linen src='../../static/img/announce/linen.png'/>
-        <Scripture src='../../static/img/announce/_Scripture.png'/>
-        </div>
-    )
+
+      <div>
+        <Background />
+      </div>
+   )
   }
 }
