821f38eb156f227385c2018359680e117278c89d
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-03-18 21:14:59 +0700

    [UPDATE] Background objrct non responsive in announced page

--- components/Announced/index.js
+++ components/Announced/index.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import Bg from '../Core/Bg'
+import Background from '../Announced/background'
+
 
 export default class componentName extends Component {
   render () {
     return (
-      <div className="container">
-      <div className="row justify-content-center">
-      <div className="col-12">
-        <img src='../../static/img/announce/_Scripture base.png'/>
-        </div>
-        </div>
-      </div>
+      <Bg>
+        <Background>
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-12">
+                <img src='../../static/img/announce/_Scripture base.png' />
+              </div>
+            </div>
+          </div>
+        </Background>
+      </Bg>
     )
   }
 }
