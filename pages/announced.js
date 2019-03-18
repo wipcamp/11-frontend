3ab5b298c94f09fe812699ae2ed82b056d2f4cd1
Unknows <firstpan555@gmail.com>
2019-03-18 13:01:35 +0700

    [ADD]Announced in pages

---
+++ pages/announced.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Announced from '../components/Announced'
+
+export default class announced extends Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Announced />
+      </React.Fragment>
+    )
+  }
+}
