cd8b12f2c434e8df0d2c840e2b9c8b0889ccf235
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-03-18 20:09:20 +0700

    [ADD] object BG in announce apge

---
+++ components/Announced/ิbackground.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+export default class componentName extends Component {
+    render () {
+      return (
+        <div>
+  
+        </div>
+      )
+    }
+  }
