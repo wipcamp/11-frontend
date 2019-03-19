957ed6dbfbb9fb0e4bb82498ecb61c2983bbfc3b
Unknows <firstpan555@gmail.com>
2019-03-19 23:39:57 +0700

    [UPDATE}Animation Scripture in Announced

--- components/Announced/Treatise.js
+++ components/Announced/Treatise.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 const Base = styled.img`
 position: absolute;
@@ -16,22 +16,32 @@ position: absolute;
 z-index: 5;
 bottom: 42.8vh;
 `
+const swing = keyframes`
+  0%, 20%, 40%, 60%, 80%, 100% {
+    transform: rotate3d(0, 0, 1, 5deg);
+  }
+  10%, 30%, 50%, 70%, 90% {
+    transform: rotate3d(0, 0, 1, -5deg);
+  }
+`
+
 const Scripture = styled.img`
 position: absolute;
 z-index: 6;
 bottom: 54vh;
+animation: ${swing} 0.5s infinite;
 `
-
+ 
 export default class componentName extends Component {
-    render () {
-        return (
+  render () {
+    return (
 
-            <div className="row justify-content-center">
-                <Base src='../../static/img/announce/_Scripture base.png' />
-                <Shadow src='../../static/img/announce/_Scripture base shadow.png' />
-                <Linen src='../../static/img/announce/linen.png' />
-                <Scripture src='../../static/img/announce/_Scripture.png' />
-            </div>
-        )
-    }
+      <div className="row justify-content-center">
+        <Base src='../../static/img/announce/_Scripture base.png' />
+        <Shadow src='../../static/img/announce/_Scripture base shadow.png' />
+        <Linen src='../../static/img/announce/linen.png' />
+        <Scripture src='../../static/img/announce/_Scripture.png' />
+      </div>
+    )
+  }
 }
