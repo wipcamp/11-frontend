7921a696496f499b3c4513a02379da43b0590c78
Unknows <firstpan555@gmail.com>
2019-03-20 11:50:20 +0700

    [UPDATE]Spin animation in announced

--- components/Announced/Treatise.js
+++ components/Announced/Treatise.js
@@ -20,12 +20,18 @@ bottom: 27vh;
 width:35%;
 `
 const swing = keyframes`
-  0%, 20%, 40%, 60%, 80%, 100% {
+  0% {
+    transform:  translate(0px,0px)  rotate(5deg) ;
+  }
+  100% {
+    transform:  translate(0px,0px)  rotate(-5deg) ;
+  }
+  /* 0%, 20%, 40%, 60%, 80%, 100% {
     transform: rotate3d(0, 0, 1, 5deg);
   }
   10%, 30%, 50%, 70%, 90% {
     transform: rotate3d(0, 0, 1, -5deg);
-  }
+  } */
 `
 
 const Scripture = styled.img`
@@ -33,11 +39,66 @@ position: absolute;
 z-index: 6;
 bottom: 38vh;
 width:20%;
-animation: ${swing} 0.5s infinite;
+/* animation: ${swing} 0.1s infinite; */
+animation: ${props => props.discription};
+@keyframes spin{
+    0% {
+        transform: translate(0, 0) rotate(0deg);
+        opacity : 1;
+    }
+    20% {
+        transform: translate(0, -100%) rotate(360deg);
+        opacity : 1;
+    }
+    40% {
+        transform: translate(0, -200%) rotate(720deg);
+        opacity : 1;
+    }
+    60% {
+        transform: translate(0, -300%) rotate(1080deg);
+        opacity : 1;
+    }
+    80% {
+        transform: translate(0, -400%) rotate(1440deg);
+        opacity : 1;
+    }
+    100% {
+        transform: translate(0, -500%) rotate(1800deg);
+        opacity : 0;
+    }
+}
+@keyframes swing{
+  0% {
+    transform:  translate(0px,0px)  rotate(5deg) ;
+  }
+  100% {
+    transform:  translate(0px,0px)  rotate(-5deg) ;
+  }
+}
 
 `
+const Pic = (props) => (
+  <Scripture discription={props.discription} src='../../static/img/announce/_Scripture.png' />
+)
  
 export default class componentName extends Component {
+  state = {
+    discription: ['swing 0.3s infinite', 'swing 0.1s 20', 'spin 2s 1 linear'],
+    count:0,
+
+  }
+  handleClick = () =>{
+  if(i== true){
+    this.setState({
+      count: 1
+    })
+  }
+  else if(i == false){
+    this.setState({
+      count : 0
+    })
+  }
+  }
   render () {
     return (
 
@@ -45,7 +106,9 @@ export default class componentName extends Component {
         <Base src='../../static/img/announce/_Scripture base.png' />
         <Shadow src='../../static/img/announce/Scripturebaseshadow.png' />
         <Linen src='../../static/img/announce/linen.png' />
-        <Scripture src='../../static/img/announce/_Scripture.png' />
+        <Pic 
+          discription={this.state.discription[this.state.count]}  
+        />
       </div>
     )
   }
