a0d450c458c01b96b72a2140884932638a17dac6
yunxyinx <yaowanat.yy@mail.kmutt.ac.th>
2019-03-21 21:41:56 +0700

    [UPDATE] Add id facebook

--- components/Announced/ButtonLogIn.js
+++ components/Announced/ButtonLogIn.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import FacebookLogin from 'react-facebook-login';
-const Button = styled.img`
+import FacebookLogin from 'react-facebook-login'
+
+const Button = styled.div`
 position: absolute;
 z-index:10;
 bottom: 50vh;
-width: 30%;
+border-radius: 5px;
 `
 const responseFacebook = (response) => {
   console.log(response);
@@ -15,16 +16,12 @@ export default class componentName extends Component {
   render () {
     return (
         <div className="row justify-content-center">
-        {/* <FacebookLogin
-          scope="email"
-          autoLoad={true}
-          fields="name,email,picture,id"
-          appId="293604811359850"
-          callback={responseFacebook}
-          render={renderProps => ( */}
-          <Button alt="Facebook Login" src='../static/img/announce/Button_Facebook.png'/>
-          {/* // )}
-          // /> */}
+        <Button>
+      <FacebookLogin
+        appId="293604811359850"
+        autoLoad={true}
+        callback={this.responseFacebook}/>
+      </Button>
       </div>
    )
   }
