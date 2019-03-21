3c414a56e9d7f52cfa438c0c527fe9eed2a71cbe
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-03-21 23:25:34 +0700

    no message

--- components/Announced/Treatise.js
+++ components/Announced/Treatise.js
@@ -66,13 +66,9 @@ width:100%;
 
 `
 
-<<<<<<< HEAD
 
 
 const Scripture = styled.div`
-=======
-const DesktopScripture = styled.div`
->>>>>>> 3f6e779f08908c032099bd7b34b6510df73ccc2b
 position: absolute;
 z-index: 6;
 bottom: 0vh;
@@ -87,12 +83,7 @@ width:100%;
 
 animation: ${props => props.discription};
 animation-fill-mode: forwards;
-@media (min-width:320px) {
-  display: none;
-}
-@media (min-width:769px) {
-  display: block;
-}
+
 @keyframes spin{
   
   0%{
@@ -144,91 +135,11 @@ animation-fill-mode: forwards;
     background-position:center;
     background-repeat:no-repeat;
     background-size:contain;
-    transform: translate(0, 0) rotate(0deg);
+    transform: ${props => props.transform};
     opacity:1;
     bottom:30vh;
-    width: 50%;
-    height: 50%;
-  }
-}
-@keyframes swing{
-  0% {
-    transform:  translate(0px,0px)  rotate(5deg) ;
-  }
-  100% {
-    transform:  translate(0px,0px)  rotate(-5deg) ;
-  }
-}
-`
-const MobileScripture = styled.div`
-position: absolute;
-z-index: 6;
-bottom: 38vh;
-width:20%;
-animation: ${props => props.discription};
-animation-fill-mode: forwards;
-@media (min-width:320px) {
-  display: block;
-}
-@media (min-width:769px) {
-  display: none;
-}
-@keyframes spin{
-  
-  0%{
-  content : url('../../static/img/announce/_Scripture.png');
-    width:auto;
-    height:auto;
-    transform: rotate3d(0, 0, 1, 5deg);
-  }
-  10%, 20%, 30%, 40%, 50% {
-  content : url('../../static/img/announce/_Scripture.png');
-    transform: rotate3d(0, 0, 1, 5deg);
-  }
-  5%, 15%, 25%, 35%, 45%, 55% {
-  content : url('../../static/img/announce/_Scripture.png');
-    transform: rotate3d(0, 0, 1, -5deg);
-  }
-  60% {
-      content : url('../../static/img/announce/_Scripture.png');
-      transform: translate(0, -100%) rotate(360deg);
-      opacity : 1;
-      bottom:54vh;
-  }
-  65% {
-      content : url('../../static/img/announce/_Scripture.png');
-      transform: translate(0, -200%) rotate(720deg);
-      opacity : 1;
-      width:auto;
-      height:auto;
-  }
-  70% {
-      content : url('../../static/img/announce/_Scripture.png');
-      transform: translate(0, -300%) rotate(1080deg);
-      opacity : 1;
-  }
-  75% {
-      content : url('../../static/img/announce/_Scripture.png');
-      transform: translate(0, -400%) rotate(1440deg);
-      opacity : 0;
-  }
-  80% {
-      content : url('../../static/img/announce/_Scripture.png');
-      transform: translate(0, -500%) rotate(1800deg);
-      opacity : 0;
-      
-  }
-  100%{
-      /* content : url('../../static/img/announce/_Scripture.png'); */
-    background-image: url('../../static/img/announce/_ScriptureFull.png');
-    background-position:center;
-    background-repeat:no-repeat;
-    background-size:contain;
-    transform:translate(0, 0) rotate(90deg);
-    opacity:1;
-    bottom:30vh;
-    width:100%;
-    height:30%;
+    width:${props => props.width || '50%'};
+    height:${props => props.height || '50%'};
     border:none;
   }
 }
@@ -254,7 +165,6 @@ const Span = styled.div`
   }
 `
 
-<<<<<<< HEAD
 const Pic = (props) => (
   <Scripture
     id="animation"
@@ -265,23 +175,6 @@ const Pic = (props) => (
   >
     <Span>{props.text}</Span>
   </Scripture>
-=======
-const Animation = (props) => (
-  <React.Fragment>
-  <DesktopScripture 
-    id="animation" 
-    discription={props.discription} 
-    >
-    <Span>{props.text}</Span>
-    </DesktopScripture>
-  <MobileScripture 
-    id="animation" 
-    discription={props.discription} 
-    >
-    <Span>{props.text}</Span>
-    </MobileScripture>
-    </React.Fragment>
->>>>>>> 3f6e779f08908c032099bd7b34b6510df73ccc2b
 )
 
 export default class Treatise extends Component {
@@ -291,11 +184,10 @@ export default class Treatise extends Component {
 
   componentDidMount = () => {
     this.performAndDisapper();
-    // window.addEventListener("resize", this.resize.bind(this));
-    // this.resize();
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
   };
 
-<<<<<<< HEAD
   resize() {
     // this.setState({ hideNav: <= 760 });
     if (window.innerWidth <= 768) {
@@ -309,21 +201,6 @@ export default class Treatise extends Component {
       })
     }
   }
-=======
-  // resize() {
-  //   // this.setState({ hideNav: <= 760 });
-  //   if (window.innerWidth  <= 768) {
-  //     this.setState({
-  //       trancount: 1
-  //     })
-  //   }
-  //   if (window.innerWidth > 768) {
-  //     this.setState({
-  //       trancount: 0
-  //     })
-  //   }
-  // }
->>>>>>> 3f6e779f08908c032099bd7b34b6510df73ccc2b
 
   componentWillUnmount = () => {
     clearTimeout(this.timeouts)
@@ -345,7 +222,6 @@ export default class Treatise extends Component {
       'swing 0.3s infinite',
       'spin 3s linear'
     ],
-<<<<<<< HEAD
     transform: [
       'translate(0, 0) rotate(0deg)', //desktop
       'translate(0, 0) rotate(90deg)' //mobile
@@ -359,21 +235,13 @@ export default class Treatise extends Component {
       '30%' //mobile
     ],
     text: [
-=======
-    text : [
->>>>>>> 3f6e779f08908c032099bd7b34b6510df73ccc2b
       'เสียใจด้วยน้องติดแล้ว',
       'ดีใจด้วยน้องไม่ติด'
     ],
     textcount: 0,
     count: 1,
-<<<<<<< HEAD
     trancount: 0,
     widthdevice: 0
-=======
-    // trancount: 0,
-    // widthdevice : 0
->>>>>>> 3f6e779f08908c032099bd7b34b6510df73ccc2b
 
   }
   handleClick = () => {
@@ -390,7 +258,6 @@ export default class Treatise extends Component {
   }
   render() {
     return (
-<<<<<<< HEAD
       <React.Fragment>
 
         <div className="row justify-content-center" id="background">
@@ -408,22 +275,6 @@ export default class Treatise extends Component {
           />
         </div>
       </React.Fragment>
-=======
-<React.Fragment>
-
-      <div className="row justify-content-center" id="background">
-        <Base src='../../static/img/announce/_Scripture base.png' />
-        <Shadow src='../../static/img/announce/Scripturebaseshadow.png' />
-        <Linen src='../../static/img/announce/linen.png' />
-      </div>
-      <div className="row justify-content-center">
-        <Animation 
-          discription={this.state.discription[this.state.count]}
-          text = {this.state.text[this.state.textcount]}
-        />
-      </div>
-</React.Fragment>
->>>>>>> 3f6e779f08908c032099bd7b34b6510df73ccc2b
     )
   }
 }
