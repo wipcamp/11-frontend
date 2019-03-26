7ea9c40e0e6f445810b0f4c50550229582bda9c9
yunxyinx <yaowanat.yy@mail.kmutt.ac.th>
2019-03-26 18:18:16 +0700

    [UPDATE] axios

--- components/Announced/ButtonLogIn.js
+++ components/Announced/ButtonLogIn.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import FacebookLogin from 'react-facebook-login'
-
+import axios from 'axios'
 const Button = styled.div`
 position: absolute;
 z-index:10;
@@ -9,25 +9,18 @@ bottom: 50vh;
 border-radius: 5px;
 display: ${props => props.display};
 `
-const Blur =styled.div`
-  position: absolute;
-  z-index: 9;
-  opacity: 0.8;
-  filter: blur(3px);
-`
 export default class componentName extends Component {
-  responseFacebook = (response) => {
-    console.log(response);
-    console.log(this.props.display)
+  responseFacebook = async (response) => {
     this.props.handleState()
-    
+    const res = await axios.get('https://registrant.service.freezer.in.th/api/testloginjaa')
+    console.log(res.data.status)
   }
   render () {
     return (
         <div className="row justify-content-center">
         <Button display={this.props.display}>
       <FacebookLogin
-        appId="293604811359850"
+        appId="583811982121484"
         autoLoad={false}
         callback={this.responseFacebook}/>
       </Button>
