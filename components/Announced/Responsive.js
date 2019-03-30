eed75689a2e5b4ddc5cd97b077b150f743632d79
Farmez_1999 <32671583+fame4458@users.noreply.github.com>
2019-03-30 12:14:05 +0700

    Update Responsive.js

--- components/Announced/Responsive.js
+++ components/Announced/Responsive.js
@@ -396,7 +396,7 @@ export const Base = styled.img`
 export const Button = styled.button`
   cursor: no-drop;
   transition-property: color;
-  background-color:#cc0044;
+  background-color:rgba(204, 0, 68,.7);
   border:none;
   display: inline-block;
   position: relative;
