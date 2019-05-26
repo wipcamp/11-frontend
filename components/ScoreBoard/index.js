faacdcda1166794629c5dbf7278aa7817e338fc1
fame <thinnapat.2542@mail.kmutt.ac.th>
2019-05-26 22:04:47 +0700

    [UPDATE] score sort

--- components/ScoreBoard/index.js
+++ components/ScoreBoard/index.js
@@ -3,24 +3,30 @@ import React, { Component } from 'react'
 class score extends Component {
   state = {
     team: [
-      { name: 'ซาบะ', score: 0 },
-      { name: 'แซลม่อน', score: 0 },
-      { name: 'ไดฟูกุ', score: 0 },
-      { name: 'โมจิ', score: 0 },
-      { name: 'วาซาบิ', score: 0 },
-      { name: 'โชยุ', score: 0 },
-      { name: 'พุดดิ้ง', score: 0 },
-      { name: 'มันม่วง', score: 0 },
-      { name: 'ซากุระ', score: 0 },
-      { name: 'มัจฉะ', score: 0 }
-    ]
+      { name: 'ซาบะ', score: 1 },
+      { name: 'แซลม่อน', score: 10000 },
+      { name: 'ไดฟูกุ', score: 220 },
+      { name: 'โมจิ', score: 333330 },
+      { name: 'วาซาบิ', score: 2 },
+      { name: 'โชยุ', score: 10 },
+      { name: 'พุดดิ้ง', score: 99 },
+      { name: 'มันม่วง', score: 40 },
+      { name: 'ซากุระ', score: 100 },
+      { name: 'มัจฉะ', score: 44 }
+    ],
+  }
+
+  handleScore = () => {
+    console.log('test')
+    this.state.team.sort((a,b) => b.score-a.score)
   }
   render() {
     return (
       <div className="container justify-content-center">
+      {this.handleScore()}
         {this.state.team.map((data, i) => {
           return (
-            <div className="row my-3">
+            <div key={i} className="row my-3">
               <div className="col-10">
                 <h1>{data.name}</h1>
               </div>
