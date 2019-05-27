f193544a8f03cac27904c99ac7e46ae9f446f07b
fame <thinnapat.2542@mail.kmutt.ac.th>
2019-05-27 11:53:38 +0700

    [UPDATE] id && 'รส'

--- components/ScoreBoard/index.js
+++ components/ScoreBoard/index.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react'
 class score extends Component {
   state = {
     team: [
-      { name: 'ซาบะ', score: 1 },
-      { name: 'แซลม่อน', score: 10000 },
-      { name: 'ไดฟูกุ', score: 220 },
-      { name: 'โมจิ', score: 333330 },
-      { name: 'วาซาบิ', score: 2 },
-      { name: 'โชยุ', score: 10 },
-      { name: 'พุดดิ้ง', score: 99 },
-      { name: 'มันม่วง', score: 40 },
-      { name: 'ซากุระ', score: 100 },
-      { name: 'มัจฉะ', score: 44 }
+      { id:1,name: 'ซาบะ', score: 1 },
+      { id:2,name: 'แซลม่อน', score: 10000 },
+      { id:3,name: 'ไดฟูกุ', score: 220 },
+      { id:4,name: 'โมจิ', score: 333330 },
+      { id:5,name: 'วาซาบิ', score: 2 },
+      { id:6,name: 'โชยุ', score: 10 },
+      { id:7,name: 'พุดดิ้ง', score: 99 },
+      { id:8,name: 'มันม่วง', score: 40 },
+      { id:9,name: 'ซากุระ', score: 100 },
+      { id:10,name: 'มัจฉะ', score: 44 }
     ],
   }
 
@@ -28,7 +28,7 @@ class score extends Component {
           return (
             <div key={i} className="row my-3">
               <div className="col-10">
-                <h1>{data.name}</h1>
+                <h1>รส{data.name}</h1>
               </div>
               <div className="col-2">
                 <h1>{data.score}</h1>
