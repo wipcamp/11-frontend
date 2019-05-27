65244383360939defb04802c615a9ddd2070bb1e
Ittidate <pepea.23@live.com>
2019-05-27 15:43:47 +0700

    [UPDATE] service

--- components/ScoreBoard/index.js
+++ components/ScoreBoard/index.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react'
-
+import service from './serviec'
 class score extends Component {
   state = {
     team: [
-      { name: 'ซาบะ', score: 1 },
-      { name: 'แซลม่อน', score: 10000 },
-      { name: 'ไดฟูกุ', score: 220 },
-      { name: 'โมจิ', score: 333330 },
-      { name: 'วาซาบิ', score: 2 },
-      { name: 'โชยุ', score: 10 },
-      { name: 'พุดดิ้ง', score: 99 },
-      { name: 'มันม่วง', score: 40 },
-      { name: 'ซากุระ', score: 100 },
-      { name: 'มัจฉะ', score: 44 }
+      { name: 'ซาบะ', score: 0 },
+      { name: 'แซลม่อน', score: 0 },
+      { name: 'ไดฟูกุ', score: 0 },
+      { name: 'โมจิ', score: 0 },
+      { name: 'วาซาบิ', score: 0 },
+      { name: 'โชยุ', score: 0 },
+      { name: 'พุดดิ้ง', score: 0 },
+      { name: 'มันม่วง', score: 0 },
+      { name: 'ซากุระ', score: 0 },
+      { name: 'มัจฉะ', score: 0 }
     ],
   }
-
+  
   handleScore = () => {
     console.log('test')
     this.state.team.sort((a,b) => b.score-a.score)
   }
+  getScore = async()=>{
+    let res = await service.getScore()
+    res=res.data
+    console.log(res)
+    res.map((data,i)=>{
+
+    })
+  }
   render() {
+    this.getScore()
     return (
       <div className="container justify-content-center">
       {this.handleScore()}
---
+++ components/ScoreBoard/serviec.js
@@ -0,0 +1,53 @@
+//this is hard code for use axios by 1 file not dynamic hahahahah
+import axios from 'axios'
+
+
+const createInstance = (headers) => {
+  return axios.create({
+    baseURL: process.env.REACT_APP_PATH_CAMPERS,
+    headers: {
+      'Authorization': `Bearer `,
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+const handleResponse = res => !res.data.error ? Promise.resolve(res) : Promise.reject(new Error(res.data.error))
+
+const catchError = err => Promise.reject(err.message)
+
+const api = {
+  get: (path, headers = {}) => (
+    createInstance(headers)
+      .get(path)
+      .then(handleResponse)
+      .catch(catchError)
+  ),
+  post: (path, body = {}, headers = {}) => (
+    createInstance(headers)
+      .request({
+        url: path,
+        method: 'POST',
+        data: body
+      })
+      .then(handleResponse)
+      .catch(catchError)
+  ),
+  put: (path, body = {}, headers = {}) => (
+    createInstance(headers)
+      .request({
+        url: path,
+        method: 'PUT',
+        data: body
+      })
+      .then(handleResponse)
+      .catch(catchError)
+  )
+}
+
+const service = {
+    getScore : async ()=>{
+      return await api.get('https://camper.service.freezer.in.th/api/test/score')
+    }
+}
+export default service;
